31812ee108154d39878d8469a8cfefa84812d227
sharper <>
2009-09-29 15:05:50 +0000

    fixed python compile error

--- HEEPAnalyzer/python/HEEPPatConfigNoPreSel_cfi.py
+++ HEEPAnalyzer/python/HEEPPatConfigNoPreSel_cfi.py
@@ -4,11 +4,9 @@ import FWCore.ParameterSet.Config as cms
 
 from SHarper.HEEPAnalyzer.HEEPPatConfig_cfi import *
 
-allLayer0Electrons.electronSource = cms.InputTag('pixelMatchGsfElectronsNoCuts')
-eleIsoDepositTk.src = cms.InputTag("pixelMatchGsfElectronsNoCuts")
-eleIsoDepositHcalFromTowers.src = cms.InputTag("pixelMatchGsfElectronsNoCuts")
-eleIsoDepositHcalDepth1FromTowers.src = cms.InputTag("pixelMatchGsfElectronsNoCuts")
-eleIsoDepositHcalDepth2FromTowers.src = cms.InputTag("pixelMatchGsfElectronsNoCuts")
-eleIsoDepositEcalFromHits.src = cms.InputTag("pixelMatchGsfElectronsNoCuts")
-
-patHEEPElectronIsolationMaps.collection = cms.InputTag("pixelMatchGsfElectronsNoCuts")
+allLayer1Electrons.electronSource = cms.InputTag('gsfElectronsNoCuts')
+eleIsoDepositTk.src = cms.InputTag("gsfElectronsNoCuts")
+eleIsoDepositHcalFromTowers.src = cms.InputTag("gsfElectronsNoCuts")
+eleIsoDepositHcalDepth1FromTowers.src = cms.InputTag("gsfElectronsNoCuts")
+eleIsoDepositHcalDepth2FromTowers.src = cms.InputTag("gsfElectronsNoCuts")
+eleIsoDepositEcalFromHits.src = cms.InputTag("gsfElectronsNoCuts")
