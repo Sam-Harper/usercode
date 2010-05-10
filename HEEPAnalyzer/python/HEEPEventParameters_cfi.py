9f1e73b236ec1acd22c9a81eb0dd05b1934c5b15
sharper <>
2010-05-10 11:42:43 +0000

    updating pat collection names

--- HEEPAnalyzer/python/HEEPEventParameters_cfi.py
+++ HEEPAnalyzer/python/HEEPEventParameters_cfi.py
@@ -6,12 +6,12 @@ heepEventPara  = cms.PSet (
     #intentionally using allLayer1 objects rather than selectedLayer1
     #because quite frankly I dont trust the selection
     #note that layer1 already has cleaning and selection cuts applied to it
-    electronTag = cms.untracked.InputTag("allLayer1Electrons"),
-    tauTag = cms.untracked.InputTag("allLayer1Taus"),
-    muonTag = cms.untracked.InputTag("allLayer1Muons"),
-    jetTag = cms.untracked.InputTag("allLayer1Jets"),
-    photonTag = cms.untracked.InputTag("allLayer1Photons"),
-    metTag = cms.untracked.InputTag("layer1METs"),
+    electronTag = cms.untracked.InputTag("patElectrons"),
+    tauTag = cms.untracked.InputTag("patTaus"),
+    muonTag = cms.untracked.InputTag("patMuons"),
+    jetTag = cms.untracked.InputTag("patJets"),
+    photonTag = cms.untracked.InputTag("patPhotons"),
+    metTag = cms.untracked.InputTag("patMETs"),
     barrelRecHitTag = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
     endcapRecHitTag = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
     reducedBarrelRecHitTag = cms.InputTag("reducedEcalRecHitsEB"),
