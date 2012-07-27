e377755ff82e1d6835a3b9e0190dd1d5fe3f3ada
sharper <>
2012-07-27 15:26:23 +0000

    adding tools to remake the PF->ele links

--- HEEPAnalyzer/python/heepTools.py
+++ HEEPAnalyzer/python/heepTools.py
@@ -16,3 +16,26 @@ def swapCollection(process,oldCollection,newCollection):
                         for tag in para:
                             if tag.getModuleLabel()==oldCollection:
                                 tag.setModuleLable(newCollection)
+
+
+def swapCollectionModuleAndProductLabels(process,oldModuleLabel,oldProdInstLabel,newModuleLabel,newProdInstLabel):
+    for pathName in process.pathNames().split():
+        path = getattr(process,pathName)
+        for moduleName in path.moduleNames():
+            if moduleName != newModuleLabel:
+                module = getattr(process,moduleName)
+                for paraName in module.parameters_():
+                    para = module.getParameter(paraName)
+                    if type(para).__name__=="InputTag":
+                        if para.getModuleLabel()==oldModuleLabel:
+                            if para.getProductInstanceLabel()==oldProdInstLabel:
+                                para.setModuleLabel(newModuleLabel)
+                                para.setProductInstanceLabel(newProdInstLabel)
+                            
+ #print paraName,type(para).__name__,para.getModuleLabel()
+                    if type(para).__name__=="VInputTag":
+                        for tag in para:
+                            if tag.getModuleLabel()==oldModuleLabel:
+                                if tag.getProductInstanceLabel()==oldProdInstLabel:
+                                    tag.setModuleLable(newModuleLable)
+                                    tag.setProductLabel(newProductLabel)
