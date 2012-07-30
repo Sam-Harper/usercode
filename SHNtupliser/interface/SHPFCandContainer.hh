b81445c4cc087d691ecbd02f88a141377c701ce0
sharper <>
2012-07-30 19:57:43 +0000

    adding more PF stuff

--- SHNtupliser/interface/SHPFCandContainer.hh
+++ SHNtupliser/interface/SHPFCandContainer.hh
@@ -12,11 +12,19 @@ private:
   std::vector<SHPFCandidate> photon_;
 
 public:
-  void addChargedHad(float pt,float eta,float phi,float mass,float mvaNothingGamma,int scSeedCrysId){chargedHad_.push_back(SHPFCandidate(pt,eta,phi,mass,mvaNothingGamma,scSeedCrysId));}
-  void addNeutralHad(float pt,float eta,float phi,float mass,float mvaNothingGamma,int scSeedCrysId){neutralHad_.push_back(SHPFCandidate(pt,eta,phi,mass,mvaNothingGamma,scSeedCrysId));}
-  void addPhoton(float pt,float eta,float phi,float mass,float mvaNothingGamma,int scSeedCrysId){photon_.push_back(SHPFCandidate(pt,eta,phi,mass,mvaNothingGamma,scSeedCrysId));}
+  SHPFCandidate& addChargedHad(float pt,float eta,float phi,float mass,float mvaNothingGamma,int scSeedCrysId){chargedHad_.push_back(SHPFCandidate(pt,eta,phi,mass,mvaNothingGamma,scSeedCrysId));return chargedHad_.back();}
+  SHPFCandidate& addNeutralHad(float pt,float eta,float phi,float mass,float mvaNothingGamma,int scSeedCrysId){neutralHad_.push_back(SHPFCandidate(pt,eta,phi,mass,mvaNothingGamma,scSeedCrysId));return neutralHad_.back();}
+  SHPFCandidate& addPhoton(float pt,float eta,float phi,float mass,float mvaNothingGamma,int scSeedCrysId){photon_.push_back(SHPFCandidate(pt,eta,phi,mass,mvaNothingGamma,scSeedCrysId));return photon_.back();}
 
   void clear();
+
+  size_t nrPhos()const{return photon_.size();}
+  size_t nrChargedHad()const{return chargedHad_.size();}
+  size_t nrNeutralHad()const{return neutralHad_.size();}
+  
+  const SHPFCandidate& photon(size_t nr)const{return photon_[nr];}
+  const SHPFCandidate& chargedHad(size_t nr)const{return chargedHad_[nr];}
+  const SHPFCandidate& neutralHad(size_t nr)const{return neutralHad_[nr];}
 
   ClassDef(SHPFCandContainer,1)
 
--- SHNtupliser/interface/SHPFCandidate.hh
+++ SHNtupliser/interface/SHPFCandidate.hh
@@ -10,18 +10,22 @@ private:
   float pt_,eta_,phi_,mass_;
   float mvaNothingGamma_;
   int scSeedCrysId_;
+  float vx_,vy_,vz_;
+
 
 public:
   SHPFCandidate():
     pt_(0.),eta_(0.),phi_(0.),mass_(0.),scSeedCrysId_(0){}
   SHPFCandidate(float iPt,float iEta,float iPhi,float iMass,float iMVANothingGamma,int iSCSeedCrysId):
-    pt_(iPt),eta_(iEta),phi_(iPhi),mass_(iMass),mvaNothingGamma_(iMVANothingGamma),scSeedCrysId_(iSCSeedCrysId){}
+    pt_(iPt),eta_(iEta),phi_(iPhi),mass_(iMass),mvaNothingGamma_(iMVANothingGamma),scSeedCrysId_(iSCSeedCrysId),vx_(0.),vy_(0.),vz_(0.){}
   
   float pt()const{return pt_;}
   float eta()const{return eta_;}
   float phi()const{return phi_;}
   float mass()const{return mass_;}
   
+  void setVertex(float x,float y,float z){vx_=x;vy_=y;vz_=z;}
+
   ClassDef(SHPFCandidate,1)
 };
 
