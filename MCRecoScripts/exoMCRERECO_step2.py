92631daf3f459d1091b30dcb70e6916e6a6bc5c7
sharper <>
2012-09-02 16:27:23 +0000

    This commit was manufactured by cvs2svn to create tag 'V00-00-01'.
    Sprout from master 2008-12-09 13:48:38 UTC sharper 'Example DeltaR plotter'
    Cherrypick from master 2012-09-02 16:27:22 UTC sharper 'should now work':
        MCRecoScripts/PUInputFiles_MinBias_TuneZ2star_8TeV_pythia6_Summer12_START50_V13_v3.py
        MCRecoScripts/crab_base.cfg
        MCRecoScripts/exoMCRERECO.sh
        MCRecoScripts/exoMCRERECO_step1.py
        MCRecoScripts/exoMCRERECO_step2.py
        MCRecoScripts/submitEXORECOJob.sh
    Cherrypick from master 2008-12-01 14:00:17 UTC sharper 'example of accessing Isolations make from IsoDeposits in 21X':
        EgIsolExample/python/eleHcalDepthSplitIsolSequence_cff.py
    Delete:
        SHNtupliser/BuildFile
        SHNtupliser/interface/DetIdTools.hh
        SHNtupliser/interface/HackedFuncs.h
        SHNtupliser/interface/LogErr.hh
        SHNtupliser/interface/LogFuncs.hh
        SHNtupliser/interface/MathFuncs.hh
        SHNtupliser/interface/SHBasicCluster.hh
        SHNtupliser/interface/SHBugFixFuncs.hh
        SHNtupliser/interface/SHCaloCellGeom.hh
        SHNtupliser/interface/SHCaloGeom.hh
        SHNtupliser/interface/SHCaloHit.hh
        SHNtupliser/interface/SHCaloHitContainer.hh
        SHNtupliser/interface/SHElectron.hh
        SHNtupliser/interface/SHEvent.hh
        SHNtupliser/interface/SHEventHelper.h
        SHNtupliser/interface/SHEvtHelper.h
        SHNtupliser/interface/SHGeomFiller.h
        SHNtupliser/interface/SHIsolCluster.hh
        SHNtupliser/interface/SHIsolSuperCluster.hh
        SHNtupliser/interface/SHIsolTrack.hh
        SHNtupliser/interface/SHJet.hh
        SHNtupliser/interface/SHMCParticle.hh
        SHNtupliser/interface/SHMet.hh
        SHNtupliser/interface/SHNtupliser.h
        SHNtupliser/interface/SHSuperCluster.hh
        SHNtupliser/interface/SHTrigInfo.hh
        SHNtupliser/interface/TempFuncs.hh
        SHNtupliser/plugins/BuildFile
        SHNtupliser/plugins/SHNtupliser.cc
        SHNtupliser/python/relVal_Zee_219_cfi.py
        SHNtupliser/python/shNtupliser_cfi.py
        SHNtupliser/src/DetIdTools.cc
        SHNtupliser/src/HackedFuncs.cc
        SHNtupliser/src/LogErr.cc
        SHNtupliser/src/LogFuncs.cc
        SHNtupliser/src/MathFuncs.cc
        SHNtupliser/src/SHBasicCluster.cc
        SHNtupliser/src/SHBasicCluster_CMSSW.cc
        SHNtupliser/src/SHBugFixFuncs.cc
        SHNtupliser/src/SHCaloCellGeom.cc
        SHNtupliser/src/SHCaloGeom.cc
        SHNtupliser/src/SHCaloHit.cc
        SHNtupliser/src/SHCaloHitContainer.cc
        SHNtupliser/src/SHElectron.cc
        SHNtupliser/src/SHElectron_CMSSW.cc
        SHNtupliser/src/SHEvent.cc
        SHNtupliser/src/SHEventHelper.cc
        SHNtupliser/src/SHEvent_CMSSW.cc
        SHNtupliser/src/SHGeomFiller.cc
        SHNtupliser/src/SHIsolCluster.cc
        SHNtupliser/src/SHIsolSuperCluster.cc
        SHNtupliser/src/SHIsolTrack.cc
        SHNtupliser/src/SHJet.cc
        SHNtupliser/src/SHJet_CMSSW.cc
        SHNtupliser/src/SHMCParticle.cc
        SHNtupliser/src/SHMet.cc
        SHNtupliser/src/SHSuperCluster.cc
        SHNtupliser/src/SHSuperCluster_CMSSW.cc
        SHNtupliser/src/SHTrigInfo.cc
        SHNtupliser/src/SHarperSHNtupliseROOTLinkDef.h
        SHNtupliser/src/TempFuncs.cc
        SHNtupliser/test/bitTest.sh
        SHNtupliser/test/crabJobKiller.sh
        SHNtupliser/test/crabKiller.sh
        SHNtupliser/test/crab_base.cfg
        SHNtupliser/test/datasets21X
        SHNtupliser/test/shNtupliser_base_cfg.py
        SHNtupliser/test/shNtupliser_cfg.py
        SHNtupliser/test/submitCrabJob.sh
        SHNtupliser/test/submitJobFromFile.sh

---
+++ MCRecoScripts/exoMCRERECO_step2.py
@@ -0,0 +1,92 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.381.2.2 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: STEP2 --step RAW2DIGI,L1Reco,RECO,VALIDATION:validation_prod,DQM:DQMOfflinePOGMC --conditions START53_V7A::All --pileup 2012_Summer_50ns_PoissonOOTPU --datamix NODATAMIXER --eventcontent AODSIM,DQM --datatier AODSIM,DQM
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mix_2012_Summer_50ns_PoissonOOTPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.Validation_cff')
+process.load('DQMOffline.Configuration.DQMOfflineMC_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+
+# Input source
+process.source = cms.Source("PoolSource",
+    secondaryFileNames = cms.untracked.vstring(),
+    fileNames = cms.untracked.vstring('file:step1Output.root')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.381.2.2 $'),
+    annotation = cms.untracked.string('STEP2 nevts:1'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.AODSIMoutput = cms.OutputModule("PoolOutputModule",
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    outputCommands = process.AODSIMEventContent.outputCommands,
+  #  fileName = cms.untracked.string('step2Output.root'),
+    fileName = cms.untracked.string('TOSED:OUTPUTFILE'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('AODSIM')
+    )
+)
+
+#I'm ignoring DQM, I'm thinking of not even outputing it
+##process.DQMoutput = cms.OutputModule("PoolOutputModule",
+##    splitLevel = cms.untracked.int32(0),
+##    outputCommands = process.DQMEventContent.outputCommands,
+##    fileName = cms.untracked.string('STEP2_RAW2DIGI_L1Reco_RECO_VALIDATION_DQM_PU_inDQM.root'),
+##    dataset = cms.untracked.PSet(
+##        filterName = cms.untracked.string(''),
+##        dataTier = cms.untracked.string('DQM')
+##    )
+##)
+
+# Additional output definition
+
+# Other statements
+process.mix.playback = True
+process.RandomNumberGeneratorService.restoreStateLabel=cms.untracked.string("randomEngineStateProducer")
+process.GlobalTag.globaltag = 'START53_V7A::All'
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.dqmoffline_step = cms.Path(process.DQMOfflinePOGMC)
+process.validation_step = cms.EndPath(process.validation_prod)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.AODSIMoutput_step = cms.EndPath(process.AODSIMoutput)
+#process.DQMoutput_step = cms.EndPath(process.DQMoutput)
+
+# Schedule definition
+#process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.validation_step,process.dqmoffline_step,process.endjob_step,process.AODSIMoutput_step,process.DQMoutput_step)
+
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.validation_step,process.dqmoffline_step,process.endjob_step,process.AODSIMoutput_step)
---
+++ MCRecoScripts/submitEXORECOJob.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+#small script to automatically submit MC RECO jobs for EXO
+
+datasetPath=$1
+nrEvents=$2
+nrJobs=$3
+#outputStorageElement=T2_UK_SGrid_RALPP
+outputStorageElement=$4 #the T2 where you want this sample
+version=$5  #a user specified version tag incase you need to re-run, defaults to v1
+
+if [ -z "$version" ];then
+    version=v1
+fi
+
+#these are the identifiers of the conditions and campange 
+#do not change
+reRECOVersion=Summer12_DR53X
+conditions=PU_S10_START53_V7A
+
+
+publishDataname=EXOMCRECO_${reRECOVersion}_${conditions}-${version}
+dbsUrlForPub="https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_02_writer/servlet/DBSServlet"
+
+#allows us to publish the data and copy to T2
+publishData=1
+copyData=1
+returnData=0
+
+datasetName=`echo $datasetPath | awk -F "/" '{print $2}'`
+outputPath=EXOMCReRECO/$reRECOVersion/$datasetName/$conditions/${version}
+outputFile=${datasetName}_${reRECOVersion}_${conditions}-${version}.root
+
+
+#subDirs=`echo $outputPath | sed 's|/|_|g'`
+subDirs=${reRECOVersion}_${conditions}-${version}
+workingDir=`echo $datasetPath | awk -F "/" '{print "crabJob_MC_"$2}' `.${subDirs}.`date +%y%m%d`_`date +%H%M%S`
+
+echo $datsetPath $nrEvents $nrJobs $outputPath $outputFile $workingDir
+
+#we use sed to edit our output files prior to submision
+sed 's|TOSED:DATASETPATH|'$datasetPath'|' crab_base.cfg | \
+sed 's|TOSED:NREVENTS|'$nrEvents'|' | \
+sed 's|TOSED:NRJOBS|'$nrJobs'|' | \
+sed 's|TOSED:OUTPUTFILE|'$outputFile'|' | \
+sed 's|TOSED:OUTPUTPATH|'$outputPath'|' | \
+sed 's|TOSED:STORAGEELEMENT|'$outputStorageElement'|' | \
+sed 's|TOSED:WORKINGDIR|'$workingDir'|' | \
+sed 's|TOSED:RETURNDATA|'$returnData'|' | \
+sed 's|TOSED:PUBLISHDATANAME|'$publishDataname'|' | \
+sed 's|TOSED:PUBLISHDATA|'$publishData'|' | \
+sed 's|TOSED:DBSURLFORPUB|'$dbsUrlForPub'|' | \
+sed 's|TOSED:COPYDATA|'$copyData'|' > crab_autoGen.cfg
+
+sed 's|TOSED:OUTPUTFILE|'$outputFile'|' exoMCRERECO.sh > exoMCRERECO_autoGen.sh
+sed 's|TOSED:OUTPUTFILE|'$outputFile'|' exoMCRERECO_step2.py > exoMCRERECO_step2_autoGen.py
+
+crab -create -cfg crab_autoGen.cfg
+crab -c $workingDir -submit
+
+ 
