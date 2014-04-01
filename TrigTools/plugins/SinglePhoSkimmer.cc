513d4ac6d538fd0129a011540b27d7cbd534268f
Sam Harper <sam.j.harper@gmail.com>
2014-04-01 12:56:25 +0200

    adding single pho skimmer

---
+++ TrigTools/plugins/SinglePhoSkimmer.cc
@@ -0,0 +1,107 @@
+
+#include "FWCore/Framework/interface/EDFilter.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "DataFormats/HLTReco/interface/TriggerEvent.h"
+
+#include "SHarper/TrigTools/interface/TrigToolsFuncs.h"
+
+class SinglePhoSkimmer : public edm::EDFilter {
+
+public:
+  
+
+private:
+  edm::InputTag trigEventTag_;
+  std::vector<std::string> singlePhoLastFilterNames_;
+  std::vector<int> preScales_;
+  int nrEtBins_;
+  float minEt_;
+  float maxEt_;
+
+public:
+  explicit SinglePhoSkimmer(const edm::ParameterSet&);
+  ~SinglePhoSkimmer(){}
+  
+  virtual void beginJob();
+  virtual bool filter(edm::Event& ,const edm::EventSetup& );
+  virtual void endJob(){}
+  
+  int getPreScale(float et);
+  size_t getBinNr(int nrBins,double min,double max,double x);
+
+};
+
+SinglePhoSkimmer::SinglePhoSkimmer(const edm::ParameterSet& iConfig)
+{
+  trigEventTag_ = iConfig.getParameter<edm::InputTag>("trigEventTag");
+  singlePhoLastFilterNames_ = iConfig.getParameter<std::vector<std::string> >("singlePhotLastFilterNames");
+  preScales_ = iConfig.getParameter<std::vector<int> >("preScales");
+  nrEtBins_ = iConfig.getParameter<int>("nrEtBins");
+  minEt_ = iConfig.getParameter<double>("minEt");
+  maxEt_ = iConfig.getParameter<double>("maxEt");
+  produces<int>("skimPreScale");
+}
+
+void SinglePhoSkimmer::beginJob()
+{ 
+
+}
+
+bool SinglePhoSkimmer::filter(edm::Event& iEvent,const edm::EventSetup& iSetup )
+{
+  edm::Handle<trigger::TriggerEvent> trigEvtHandle;
+  iEvent.getByLabel(trigEventTag_,trigEvtHandle);
+  
+  //now need to find max et object passing a single photon trigger
+  //brute force (assumes no sorting or clever ordering of paths) for the win!
+  float maxEt=-1;
+  for(size_t filterNr=0;filterNr<singlePhoLastFilterNames_.size();filterNr++){
+    
+    std::vector<math::XYZTLorentzVector> p4s;
+    trigtools::getP4sOfObsPassingFilter(p4s,(*trigEvtHandle),singlePhoLastFilterNames_[filterNr],trigEventTag_.process());
+    
+    for(size_t objNr=0;objNr<p4s.size();objNr++){
+      if(p4s[objNr].Et()>maxEt) maxEt = p4s[objNr].Et();
+    }
+  }
+
+  int preScale = getPreScale(maxEt);
+
+  std::auto_ptr<int> preScalePointer(new int);
+  *preScalePointer = preScale;
+  iEvent.put(preScalePointer);
+
+  return iEvent.id().event()%preScale;
+
+}
+
+int SinglePhoSkimmer::getPreScale(float et)
+{
+  size_t binNr=getBinNr(nrEtBins_,minEt_,maxEt_,et);
+  if(binNr<preScales_.size()) return preScales_[binNr];
+  else return 1;
+}
+
+size_t SinglePhoSkimmer::getBinNr(int nrBins,double min,double max,double x)
+{
+  //uses root logic
+  const float step = (max-min)/nrBins;
+
+  //first over, underflow
+  if(x<min) return 0; 
+  if(x>=max) return nrBins+1;
+  
+  for(int binNr=1;binNr<=nrBins;binNr++){
+    float lowEdge = step*(binNr-1)+min;
+    float highEdge = step*(binNr)+min;
+    if(x>=lowEdge && x<highEdge) return binNr;
+  }
+  return nrBins+2;
+}
+
+DEFINE_FWK_MODULE(SinglePhoSkimmer);
---
+++ TrigTools/test/singlePhoSkimmer.py
@@ -0,0 +1,89 @@
+# Import configurations
+import FWCore.ParameterSet.Config as cms
+
+# set up process
+process = cms.Process("PhoSkim")
+
+# initialize MessageLogger and output report
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkSummary = cms.untracked.PSet(
+    reportEvery = cms.untracked.int32(500),
+    limit = cms.untracked.int32(10000000)
+)
+process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
+    reportEvery = cms.untracked.int32(500),
+    limit = cms.untracked.int32(10000000)
+)
+
+import sys
+filePrefex="file:"
+if(sys.argv[2].find("/pnfs/")==0):
+    filePrefex="dcap://heplnx209.pp.rl.ac.uk:22125"
+
+if(sys.argv[2].find("/store/")==0):
+    filePrefex=""
+
+if(sys.argv[2].find("/castor/")==0):
+    filePrefex="rfio:"
+process.source = cms.Source("PoolSource",
+                        #  fileNames = cms.untracked.vstring(filePrefex+sys.argv[2]),
+                       #     inputCommands = cms.untracked.vstring("drop *","keep *_source_*_*"),
+                            fileNames = cms.untracked.vstring(),
+)
+for i in range(2,len(sys.argv)-1):
+    print filePrefex+sys.argv[i]
+    process.source.fileNames.extend([filePrefex+sys.argv[i],])
+
+
+# Additional output definition
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+process.skimHLTFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+process.skimHLTFilter.HLTPaths = cms.vstring("HLT_Photon30_CaloIdVL_v*","HLT_Photon50_CaloIdVL_v*","HLT_Photon75_CaloIdVL_v*","HLT_Photon90_CaloIdVL_v*","HLT_Photon125_v*","HLT_Photon135_v*","HLT_Photon150_v*")
+process.skimHLTFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
+
+# set the number of events
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.singlePhoFilter = cms.EDFilter("SinglePhoSkimmer",
+                                    trigEventTag_ = cms.InputTag("hltTriggerSummaryAOD","","HLT"),
+                                    singlePhoLastFilterNames = cms.vstring("hltPhoton150HEFilter","hltPhoton135HEFilter","hltPhoton125HEFilter","hltPhoton90CaloIdVLHEFilter","hltPhoton75CaloIdVLHEFilter","hltPhoton50CaloIdVLHEFilter","hltEG30CaloIdVLHEFilter"),
+                                    nrEtBins = cms.int32(2000),
+                                    minEt=cms.double(0),
+                                    maxEt=cms.double(2000),
+                                    preScales=cms.vint32(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,27,25,23,21,19,17,16,14,13,12,11,9,9,8,7,7,6,5,5,4,38,36,32,30,27,26,23,22,20,19,18,16,15,14,13,12,11,11,10,10,9,8,8,7,7,37,35,33,31,29,28,26,24,23,22,21,20,19,17,17,45,42,41,38,37,34,33,32,30,28,27,26,24,23,22,21,20,19,19,18,17,16,15,15,14,14,13,13,12,12,11,11,10,10,9,9,9,8,8,8,7,7,7,7,6,355,342,329,318,307,294,284,275,265,255,247,237,231,225,218,210,204,197,190,185,179,173,167,162,158,153,148,144,140,136,132,128,124,120,117,114,110,107,105,102,98,96,92,90,88,84,83,81,79,76,74,72,70,68,66,65,62,61,59,58,56,55,52,51,51,49,48,46,45,44,43,42,40,39,38,37,37,35,35,34,33,31,31,30,30,29,28,27,26,26,25,25,24,23,23,22,22,21,20,20,19,19,18,18,18,17,17,16,16,16,15,15,14,14,14,13,13,12,12,12,12,12,11,11,11,10,10,10,10,9,9,9,9,9,8,8,8,8,8,7,7,7,7,7,7,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                                         )
+
+)
+
+process.skimP = cms.Path(process.skimHLTFilter*process.singlePhoFilter)    
+process.load('Configuration/EventContent/EventContent_cff')
+process.output = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+   outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
+#outputCommands = process.RECOEventContent.outputCommands,
+    #outputCommands = cms.untracked.vstring("keep *"),
+  fileName = cms.untracked.string("dummy.root"),
+  SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('skimP')),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('HLTDEBUG'),
+  #      filterName = cms.untracked.string('JETMETFilter'),
+        
+                                     
+    )
+)
+
+isCrabJob=False
+
+#if 1, its a crab job...
+if isCrabJob:
+    print "using crab specified filename"
+    process.output.fileName= "OUTPUTFILE"
+    
+else:
+    print "using user specified filename"
+    process.output.fileName= sys.argv[len(sys.argv)-1]
+
+
+process.HLTOutput_sam = cms.EndPath(process.output)
