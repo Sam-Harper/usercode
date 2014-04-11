45c42691a7f6121906428aa130d639c66a06edf6
Sam Harper <sam.j.harper@gmail.com>
2014-04-11 14:12:55 +0200

    making a branch with only the reco scripts

--- HEEPAnalyzer/python/HEEPFilter_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from SHarper.HEEPAnalyzer.HEEPEventParameters_cfi import *
-
-heepFilter = cms.EDFilter("HEEPFilter",
-    heepEventPara                   
-)
-
-
