3025c83ea96766763df87bddb6914f824bfdf2ac
Sam Harper <sam.j.harper@gmail.com>
2015-01-09 13:49:01 +0000

    config changes

--- SHNtupliser/test/shL1Ntupliser_stage1FromRAW.py
+++ SHNtupliser/test/shL1Ntupliser_stage1FromRAW.py
@@ -1,63 +1,39 @@
-# Auto generated configuration file
-# using: 
-# Revision: 1.19 
-# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: l1 -s L1 --pileup=NoPileUp --geometry DB --conditions=auto:startup -n 1 --no_exec --filein=/store/mc/Fall13dr/TT_Tune4C_13TeV-pythia8-tauola/GEN-SIM-RAW/tsg_PU40bx25_POSTLS162_V2-v1/00000/007939EF-8075-E311-B675-0025905938AA.root
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('L1')
 
-# import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
-process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
 process.load('Configuration.EventContent.EventContent_cff')
-process.load('SimGeneral.MixingModule.mixNoPU_cfi')
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_cff')
-process.load('Configuration.StandardSequences.SimL1Emulator_cff')
-process.load('Configuration.StandardSequences.EndOfProcess_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.Geometry.GeometryIdeal_cff')
+
+
+# Select the Message Logger output you would like to see:
+process.load('FWCore.MessageService.MessageLogger_cfi')
+
 
 process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
     reportEvery = cms.untracked.int32(1000),
     limit = cms.untracked.int32(1000)
 )
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(-1)
 )
-import sys
+
+import sys  
 # Input source
-filePrefex="file:"
-if(sys.argv[2].find("/pnfs/")==0):
-    filePrefex="dcap://heplnx209.pp.rl.ac.uk:22125"
 
-if(sys.argv[2].find("/store/")==0):
-    filePrefex=""
-if(sys.argv[2].find("/castor/")==0):
-    filePrefex="rfio:"
 process.source = cms.Source("PoolSource",
-                 #         fileNames = cms.untracked.vstring(filePrefex+sys.argv[2]),
-                       #     inputCommands = cms.untracked.vstring("drop *","keep *_source_*_*"),
+                            #         fileNames = cms.untracked.vstring(filePrefex+sys.argv[2]),
+                            #     inputCommands = cms.untracked.vstring("drop *","keep *_source_*_*"),
                             fileNames = cms.untracked.vstring(),
-)
-for i in range(2,len(sys.argv)-1):
-    print filePrefex+sys.argv[i]
-    process.source.fileNames.extend([filePrefex+sys.argv[i],])
-process.options = cms.untracked.PSet(
-
-)
+                            )
 
 
 process.options = cms.untracked.PSet(
 
-)
-
-# Production Info
-process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.19 $'),
-    annotation = cms.untracked.string('l1 nevts:1'),
-    name = cms.untracked.string('Applications')
 )
 
 # Output definition
@@ -76,15 +52,19 @@ process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
 # Additional output definition
 
 # Other statements
-from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:startup', '')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag as customiseGlobalTag
+process.GlobalTag = customiseGlobalTag(process.GlobalTag, globaltag = 'MCRUN2_72_V3A')
+#process.GlobalTag = GlobalTag(process.GlobalTag, globaltag ='MCRUN2_72_V3A')
 
-
-# to get input digis
-process.load('Configuration.StandardSequences.RawToDigi_cff')
-
-# upgrade calo stage 1
 process.load('L1Trigger.L1TCalorimeter.L1TCaloStage1_PPFromRaw_cff')
+
+
+# GT
+from L1Trigger.Configuration.SimL1Emulator_cff import simGtDigis
+process.simGtDigis = simGtDigis.clone()
+process.simGtDigis.GmtInputTag = 'simGmtDigis'
+process.simGtDigis.GctInputTag = 'caloStage1LegacyFormatDigis'
+process.simGtDigis.TechnicalTriggersInputTags = cms.VInputTag( )
 
 
 from SHarper.HEEPAnalyzer.HEEPEventParameters_cfi import *
@@ -102,6 +82,8 @@ process.shNtupliser.trigResultsTag = cms.InputTag("TriggerResults","","DontActua
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string("output.root")
 )
+
+process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Geometry.CaloEventSetup.CaloTowerConstituents_cfi")
 
 isCrabJob=False #script seds this if its a crab job
@@ -119,6 +101,21 @@ else:
     #process.shNtupliser.outputFilename= sys.argv[len(sys.argv)-1]
     process.shNtupliser.datasetCode = 101
     process.shNtupliser.sampleWeight = 1
+
+
+    filePrefex="file:"
+    if(sys.argv[2].find("/pnfs/")==0):
+        filePrefex="dcap://heplnx209.pp.rl.ac.uk:22125"
+        
+    if(sys.argv[2].find("/store/")==0):
+        filePrefex=""
+    if(sys.argv[2].find("/castor/")==0):
+        filePrefex="rfio:"
+
+    for i in range(2,len(sys.argv)-1):
+        print filePrefex+sys.argv[i]
+        process.source.fileNames.extend([filePrefex+sys.argv[i],])
+
 
 process.stage1L1extraParticles = cms.EDProducer("L1ExtraParticlesProd",
                                           muonSource = cms.InputTag("gcDigis"),
@@ -146,15 +143,22 @@ process.mcFilter = cms.EDFilter("MCTruthFilter",
                                 )
 
 
-
-# Path and EndPath definitions
 process.L1simulation_step = cms.Path(
-    process.ecalDigis
-    +process.hcalDigis
-    +process.L1TCaloStage1_PPFromRaw
+    process.L1TCaloStage1_PPFromRaw
+    +process.simGtDigis
+    #+process.l1ExtraLayer2
     +process.stage1L1extraParticles
     +process.shNtupliser
-)
+    )
+
+### Path and EndPath definitions
+##process.L1simulation_step = cms.Path(
+##    process.ecalDigis
+##    +process.hcalDigis
+##    +process.L1TCaloStage1_PPFromRaw
+##    +process.stage1L1extraParticles
+##    +process.shNtupliser
+##)
 
 if process.shNtupliser.datasetCode==122:
     print "DYToLL MC detected, filtering based on electron decays"
@@ -163,6 +167,6 @@ if process.shNtupliser.datasetCode==122:
 #process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.L1simulation_step)#,
+#process.schedule = cms.Schedule(process.L1simulation_step)#,
                          #       process.RECOSIMoutput_step)
 
