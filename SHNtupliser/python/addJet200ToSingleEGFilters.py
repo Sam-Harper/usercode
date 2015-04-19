e5291531a1e1a04711fc3c40b11aa4d0214ce5a4
Sam Harper <sam.j.harper@gmail.com>
2015-04-19 14:13:35 +0100

    adding warning messsage

--- SHNtupliser/python/addJet200ToSingleEGFilters.py
+++ SHNtupliser/python/addJet200ToSingleEGFilters.py
@@ -1,15 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 def addJet200ToSingleEGFilters(process):
-    
+    print "turning on EG Jet Seeding"
     for filterName in process.filterNames().split():
         if filterName.find("hltL1sL1SingleEG")==0:
             filt = getattr(process,filterName)
             if filt.hasParameter("L1SeedsLogicalExpression"):
                 param=filt.getParameter("L1SeedsLogicalExpression")
                 param.setValue(param.value()+" OR L1_SingleJet200")
+            #    param.setValue("L1_SingleJet200")
                 print "changed ",filterName, "to ",param.value()
            # else:
            #     print filterName,"has no save tags field"
+  
     process.hltRechitInRegionsECAL = cms.EDProducer("HLTEcalRecHitInAllL1RegionsProducer",
                                                     recHitLabels = cms.VInputTag( 'hltEcalRecHit:EcalRecHitsEB','hltEcalRecHit:EcalRecHitsEE' ),
                                                     productLabels = cms.vstring( 'EcalRecHitsEB','EcalRecHitsEE' ),
