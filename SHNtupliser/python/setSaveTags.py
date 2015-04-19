58d2363ba8f9d3225bf5eef7716ddce75fdc4113
Sam Harper <sam.j.harper@gmail.com>
2015-04-19 14:14:16 +0100

    adding new func

--- SHNtupliser/python/setSaveTags.py
+++ SHNtupliser/python/setSaveTags.py
@@ -8,3 +8,15 @@ def setSaveTags(process,pathName,saveTagsValue):
                 param.setValue(saveTagsValue)
            # else:
            #     print filterName,"has no save tags field"
+
+def printSaveTagFilterNames(process,pathName):
+    print "PATH: ",pathName
+    path = getattr(process,pathName)
+    for filterName in path.moduleNames():
+        filt = getattr(process,filterName)
+        if type(filt).__name__=="EDFilter":
+            if filt.hasParameter("saveTags"):
+                param=filt.getParameter("saveTags")
+                if param.value()==True:
+                    print filterName
+                    #param.setValue(saveTagsValue)
