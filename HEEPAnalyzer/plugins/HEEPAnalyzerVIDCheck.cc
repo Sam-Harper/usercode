64f62b52ce81a9bb6cb3a5a2a7612dfcea19ec46
Sam Harper <sam.j.harper@gmail.com>
2015-05-22 10:02:10 +0200

    adding in new value map producer

---
+++ HEEPAnalyzer/plugins/HEEPAnalyzerVIDCheck.cc
@@ -0,0 +1,90 @@
+#include "SHarper/HEEPAnalyzer/interface/HEEPAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+
+#include "TH1D.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+
+//delete these
+#include "DataFormats/RecoCandidate/interface/IsoDepositFwd.h"
+#include "DataFormats/RecoCandidate/interface/IsoDeposit.h"
+#include "SHarper/HEEPAnalyzer/interface/HEEPDebug.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h" 
+
+HEEPAnalyzer::HEEPAnalyzer(const edm::ParameterSet& iPara):
+  evtHelper_(),heepEvt_(),nrPass_(0),nrFail_(0)
+{
+  //we now have to set up the heep event
+  evtHelper_.setup(iPara);
+
+
+}
+
+void HEEPAnalyzer::beginJob()
+{
+  edm::Service<TFileService> fs;
+  massHist_ = fs->make<TH1D>("massHist","Di-Electron Mass;M_{ee} (GeV/c^{2});# Events / 10 GeV/c^{2}",250,50,2550); 
+}
+
+void HEEPAnalyzer::analyze(const edm::Event& iEvent,const edm::EventSetup& iSetup)
+{ 
+  //make the heep event (see easy isnt it)
+  evtHelper_.makeHeepEvent(iEvent,iSetup,heepEvt_);
+
+
+  //do what ever you want
+  //lets get the heep electrons and count the number that pass / fail cuts
+  const std::vector<heep::Ele>& eles = heepEvt_.heepEles();
+  for(size_t eleNr=0;eleNr<eles.size();eleNr++){     
+    bool passVid = (*heepEvt_.handles().heepIDVID)[eles[eleNr].gsfEleEDMPtr()];
+    bool passHeep = eles[eleNr].cutCode()==0x0;
+    edm::Handle<edm::ValueMap<unsigned> > howFarHandle;
+    iEvent.getByLabel(edm::InputTag("egmGsfElectronIDs","heepElectronID-HEEPV51-miniAOD"),howFarHandle);
+    size_t howFar = (*howFarHandle)[eles[eleNr].gsfEleEDMPtr()];
+    const heep::Ele& ele = eles[eleNr];
+    if(passVid!=passHeep){
+      std::cout <<" disgreement vid "<<passVid<<" got to "<<howFar<< " heep "<<heep::CutCodes::getCodeName(ele.cutCode())<<" et "<<ele.et()<<" eta "<<ele.eta()<<" phi "<<ele.phi()<<" e2x5 "<<ele.e2x5MaxOver5x5Full5x5()<<" e1x5 "<<ele.e1x5Over5x5Full5x5()<<" hadem "<<ele.gsfEle().hadronicOverEm()<<" had "<<ele.gsfEle().hadronicOverEm()*ele.superCluster().energy()<<" had cut "<<2+0.05*ele.superCluster().energy()<<" "<<heepEvt_.runnr()<<" "<<heepEvt_.eventnr()<<std::endl;
+    }
+    // std::cout <<"eleNr "<<eleNr<< "pass "<<<<" passVID "<<passVid<<std::endl;
+    if(eles[eleNr].cutCode()==0x0) nrPass_++;
+    else nrFail_++;
+  }
+
+
+  //lets fill a mass hist
+  for(size_t ele1Nr=0;ele1Nr<eles.size();ele1Nr++){
+    for(size_t ele2Nr=ele1Nr+1;ele2Nr<eles.size();ele2Nr++){
+      const heep::Ele& ele1 = eles[ele1Nr];
+      const heep::Ele& ele2 = eles[ele2Nr];  
+	//	std::cout <<"ele "<<ele1Nr<<" cutcode "<<std::hex<<ele1.cutCode()<<std::dec<<" ele "<<ele2Nr<<" cut code "<<std::hex<<ele2.cutCode()<<std::dec<<" ee "<<ele1.isEE()<<" 2nd ele "<<ele2.isEE()<<std::endl;
+      
+      if(ele1.cutCode()==0x0 && ele2.cutCode()==0x0 && !(ele1.isEE() && ele2.isEE())){ //EB-EB, EE-EE
+	//	std::cout <<"runnr "<<heepEvt_.runnr()<<" eventnr "<<heepEvt_.eventnr()<<std::endl;
+	float mass = (ele1.p4()+ele2.p4()).mag();
+	massHist_->Fill(mass);
+      }
+    }
+  }
+  
+}
+
+//function cross-checks that code that should give identical results does give identical results
+//void HEEPAnalyzer::validate()
+//{
+  
+
+
+//}
+
+void HEEPAnalyzer::endJob()
+{
+  edm::LogInfo("HEEPAnalyzer") <<"Nr pass "<<nrPass_<<" nr fail "<<nrFail_<<" eff "<<static_cast<float>(nrPass_)/static_cast<float>(nrFail_+nrPass_);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(HEEPAnalyzer);
---
+++ HEEPAnalyzer/plugins/HEEPIdAndVIDComp.cc
@@ -0,0 +1,58 @@
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/Utilities/interface/EDGetToken.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/Common/interface/Ptr.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "DataFormats/Common/interface/ValueMap.h"
+#include "SHarper/HEEPAnalyzer/interface/HEEPCutCodes.h"
+class HEEPIdAndVIDComp : public edm::EDAnalyzer {
+
+private:
+ 
+  edm::EDGetTokenT<edm::View<reco::GsfElectron> > eleToken_;
+  edm::EDGetTokenT<edm::ValueMap<int> > heepIdToken_;
+  edm::EDGetTokenT<edm::ValueMap<bool> > vidToken_;
+
+public:
+  explicit HEEPIdAndVIDComp(const edm::ParameterSet& iPara);
+  virtual ~HEEPIdAndVIDComp(){}
+  
+private:
+  void analyze(const edm::Event& iEvent,const edm::EventSetup& iSetup) override;
+ 
+};
+  
+
+HEEPIdAndVIDComp::HEEPIdAndVIDComp(const edm::ParameterSet& iPara)
+{
+  eleToken_=consumes<edm::View<reco::GsfElectron> >(iPara.getParameter<edm::InputTag>("eleLabel"));
+  heepIdToken_=consumes<edm::ValueMap<int> >(iPara.getParameter<edm::InputTag>("heepId"));
+  vidToken_=consumes<edm::ValueMap<bool> >(iPara.getParameter<edm::InputTag>("vid"));
+  
+}
+
+
+void HEEPIdAndVIDComp::analyze(const edm::Event& iEvent,const edm::EventSetup& iSetup)
+{
+  edm::Handle<edm::View<reco::GsfElectron> > eleHandle;
+  edm::Handle<edm::ValueMap<int> > heepId;
+  edm::Handle<edm::ValueMap<bool> > vid;
+
+  iEvent.getByToken(eleToken_,eleHandle);
+  iEvent.getByToken(heepIdToken_,heepId);
+  iEvent.getByToken(vidToken_,vid);
+
+  for(size_t eleNr=0;eleNr<eleHandle->size();eleNr++){  
+    edm::Ptr<reco::GsfElectron> elePtr(eleHandle,eleNr);
+    bool passVid=(*vid)[elePtr];
+    int heepBits =(*heepId)[elePtr];
+    bool passHEEP=heepBits==0x0;
+    if(passVid!=passHEEP){
+      std::cout <<"VID - HEEP disagreement, vid "<<passVid<<" heep "<<heep::CutCodes::getCodeName(heepBits)<<std::endl;
+    }
+  }
+}
+DEFINE_FWK_MODULE(HEEPIdAndVIDComp);
