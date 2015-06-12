c7e6081e5e4891b64fbedd45e46591c38a115c4a
Sam Harper <sam.j.harper@gmail.com>
2015-06-12 07:32:50 +0100

    removing unnecessary packages

--- HEEPAnalyzer/python/gsfElectronsHEEPCorr_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-gsfElectronsHEEPCorr = cms.EDProducer("HEEPGsfEleEnergyCorrector",
-                                      inputEleTag = cms.InputTag("gsfElectrons"),
-                                      useSuperClusterEnergy = cms.bool(True)
-                                      )
-
