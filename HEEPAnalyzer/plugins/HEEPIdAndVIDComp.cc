1622d43b648fc3d10111f586217eb375848c60bd
Sam Harper <sam.j.harper@gmail.com>
2016-04-15 16:59:39 +0100

    adding VID HEEP V6.1 check

--- HEEPAnalyzer/plugins/HEEPIdAndVIDComp.cc
+++ HEEPAnalyzer/plugins/HEEPIdAndVIDComp.cc
@@ -15,7 +15,7 @@ private:
   edm::EDGetTokenT<edm::View<reco::GsfElectron> > eleToken_;
   edm::EDGetTokenT<edm::ValueMap<int> > heepIdToken_;
   edm::EDGetTokenT<edm::ValueMap<bool> > vidToken_;
-
+  edm::EDGetTokenT<double>  eleRhoCorrToken_;
 public:
   explicit HEEPIdAndVIDComp(const edm::ParameterSet& iPara);
   virtual ~HEEPIdAndVIDComp(){}
@@ -31,7 +31,24 @@ HEEPIdAndVIDComp::HEEPIdAndVIDComp(const edm::ParameterSet& iPara)
   eleToken_=consumes<edm::View<reco::GsfElectron> >(iPara.getParameter<edm::InputTag>("eleLabel"));
   heepIdToken_=consumes<edm::ValueMap<int> >(iPara.getParameter<edm::InputTag>("heepId"));
   vidToken_=consumes<edm::ValueMap<bool> >(iPara.getParameter<edm::InputTag>("vid"));
+  eleRhoCorrToken_=consumes<double>(iPara.getParameter<edm::InputTag>("rho"));
+}
+
+bool heepTrkPtCut(const reco::GsfElectron& ele,double rho)
+{
+  const float startEtEB = 95.;
+  const float startEtEE = 100.;
+  const float rhoEAEB=1.5;
+  const float rhoEAEE=0.5;
   
+  const bool isEB = std::abs(ele.superCluster()->eta())<1.5;
+  const float startEt = isEB ? startEtEB :startEtEE;
+  const float rhoEA = isEB ? rhoEAEB : rhoEAEE;
+
+  const float rhoCorr = ele.et()>=startEt ? rhoEA*rho : 0.;
+
+  return ele.dr03TkSumPt()< 5 + rhoCorr;
+
 }
 
 
@@ -45,13 +62,20 @@ void HEEPIdAndVIDComp::analyze(const edm::Event& iEvent,const edm::EventSetup& i
   iEvent.getByToken(heepIdToken_,heepId);
   iEvent.getByToken(vidToken_,vid);
 
+  edm::Handle<double> rhoHandle;
+  iEvent.getByToken(eleRhoCorrToken_,rhoHandle);
+  double rho = *rhoHandle;// rhoHandle.isValid() ? *rhoHandle : 0;
+
   for(size_t eleNr=0;eleNr<eleHandle->size();eleNr++){  
     edm::Ptr<reco::GsfElectron> elePtr(eleHandle,eleNr);
     bool passVid=(*vid)[elePtr];
     int heepBits =(*heepId)[elePtr];
-    bool passHEEP=heepBits==0x0;
-    if(passVid!=passHEEP){
-      std::cout <<"VID - HEEP disagreement, vid "<<passVid<<" heep "<<heep::CutCodes::getCodeName(heepBits)<<std::endl;
+    bool passHEEP=(heepBits&(~heep::CutCodes::ISOLPTTRKS))==0x0;// && heepTrkPtCut(*elePtr,rho);
+    bool passHEEPTrk = heepTrkPtCut(*elePtr,rho);
+    if(passVid!=(passHEEP && passHEEPTrk)){
+      std::cout <<"VID - HEEP disagreement, vid "<<passVid<<" heep "<<heep::CutCodes::getCodeName(heepBits)<<" heepIsolTrk "<<passHEEPTrk<<" ele et "<<elePtr->et()<<" eta "<<elePtr->eta()<<std::endl;
+      
+      std::cout <<"     trk isol "<<elePtr->dr03TkSumPt()<<" rho "<<rho<<std::endl;
     }
   }
 }
--- HEEPAnalyzer/test/HEEPAnalyzerVsVIDCheck_cfg.py
+++ HEEPAnalyzer/test/HEEPAnalyzerVsVIDCheck_cfg.py
@@ -9,92 +9,111 @@ process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
 )
 
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 # NOTE: the pick the right global tag!
-#    for PHYS14 scenario PU4bx50 : global tag is ???
-#    for PHYS14 scenario PU20bx25: global tag is PHYS14_25_V1
-#  as a rule, find the global tag in the DAS under the Configs for given dataset
-process.GlobalTag.globaltag = 'PHYS14_25_V1'
+#    for Spring15 50ns MC: global tag is 'auto:run2_mc_50'
+#    for Spring15 25ns MC: global tag is 'auto:run2_mc'
+#    for Run 2 data: global tag is 'auto:run2_data'
+#  as a rule, find the "auto" global tag in $CMSSW_RELEASE_BASE/src/Configuration/AlCa/python/autoCond.py
+#  This auto global tag will look up the "proper" global tag
+#  that is typically found in the DAS under the Configs for given dataset
+#  (although it can be "overridden" by requirements of a given release)
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 
 #
 # Define input data to read
 #
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring(
-   #
-   # Just a handful of files from the dataset are listed below, for testing
-   #
-       '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/0432E62A-7A6C-E411-87BB-002590DB92A8.root',
-      '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/06C61714-7E6C-E411-9205-002590DB92A8.root',
-       '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/0EAD09A8-7C6C-E411-B903-0025901D493E.root'
- ),
-              #               eventsToProcess = cms.untracked.VEventRange("1:2862883-1:2862883")
-                             
-)
 
 
+inputFilesAOD = cms.untracked.vstring(
+    # AOD test files from /DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/AODSIM
+       '/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/002F7FDD-BA13-E511-AA63-0026189437F5.root',
+       '/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/00610EE7-C213-E511-842C-00304833529A.root',
+       '/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/00623DCC-A813-E511-A302-0025905B85A2.root',
+    )    
+
+inputFilesMiniAOD = cms.untracked.vstring(
+    # MiniAOD test files from /DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM
+#    '/store/mc/RunIIFall15MiniAODv1/ZToEE_NNPDF30_13TeV-powheg_M_2300_3500/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/10000/201866B5-EBB0-E511-9D50-008CFAF06942.root',
+    '/store/mc/RunIIFall15MiniAODv2/ZToEE_NNPDF30_13TeV-powheg_M_2300_3500/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/60000/84571177-6AB8-E511-B503-002590E505FE.root',
+    '/store/mc/RunIIFall15MiniAODv2/ZToEE_NNPDF30_13TeV-powheg_M_200_400/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/70000/28640CCE-8FB8-E511-B8FC-001EC9ADE609.root',
+    '/store/mc/RunIIFall15MiniAODv2/ZToEE_NNPDF30_13TeV-powheg_M_50_120/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/80000/1E4876E3-05D5-E511-85B9-44A84225C7BB.root',
+    '/store/mc/RunIIFall15MiniAODv2/ZToEE_NNPDF30_13TeV-powheg_M_50_120/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/80000/20CB3216-05D5-E511-851E-002590D8C740.root',
+    )
+
+# Set up input/output depending on the format
+# You can list here either AOD or miniAOD files, but not both types mixed
+#
+useAOD = False
+
+if useAOD == True :
+    inputFiles = inputFilesAOD
+    outputFile = "electron_ntuple.root"
+    print("AOD input files are used")
+else :
+    inputFiles = inputFilesMiniAOD
+    outputFile = "electron_ntuple_mini.root"
+    print("MiniAOD input files are used")
+process.source = cms.Source ("PoolSource", fileNames = inputFiles )                             
 
 #
-# START ELECTRON ID SECTION
-#
-# Set up everything that is needed to compute electron IDs and
-# add the ValueMaps with ID decisions into the event data stream
+# Set up electron ID (VID framework)
 #
 
-# Load tools and function definitions
 from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+# turn on VID producer, indicate data format  to be
+# DataFormat.AOD or DataFormat.MiniAOD, as appropriate 
+if useAOD == True :
+    dataFormat = DataFormat.AOD
+else :
+    dataFormat = DataFormat.MiniAOD
 
-process.load("RecoEgamma.ElectronIdentification.egmGsfElectronIDs_cfi")
-# overwrite a default parameter: for miniAOD, the collection name is a slimmed one
-process.egmGsfElectronIDs.physicsObjectSrc = cms.InputTag('slimmedElectrons')
+switchOnVIDElectronIdProducer(process, dataFormat)
 
-from PhysicsTools.SelectorUtils.centralIDRegistry import central_id_registry
-process.egmGsfElectronIDSequence = cms.Sequence(process.egmGsfElectronIDs)
+# define which IDs we want to produce
+my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_25ns_V1_cff',
+                 'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV60_cff',
+                 'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV61_cff']
 
-# Define which IDs we want to produce
-# Each of these two example IDs contains all four standard 
-# cut-based ID working points (only two WP of the PU20bx25 are actually used here).
-#my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_PHYS14_PU20bx25_V0_miniAOD_cff']
-##Add them to the VID producer
-#for idmod in my_id_modules:
-#    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+#add them to the VID producer
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
 
-#heep works differently because mini-aod and aod are defined in the same file to ensure consistancy (you cant change cuts of aod without changing miniaod
-process.load('RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV51_cff')
-setupVIDSelection(process.egmGsfElectronIDs,process.heepElectronID_HEEPV51_miniAOD)
-
-# Do not forget to add the egmGsfElectronIDSequence to the path,
-# as in the example below!
-
-#
-# END ELECTRON ID SECTION
-#
-
-#
-# Configure an example module for user analysis of electrons
-#
-process.load("RecoEgamma.ElectronIdentification.ElectronIDValueMapProducer_cfi")
-
-process.electronIDValueMapProducer.ebReducedRecHitCollection=cms.InputTag("reducedEgamma","reducedEBRecHits")
-process.electronIDValueMapProducer.eeReducedRecHitCollection=cms.InputTag("reducedEgamma","reducedEERecHits")
-process.electronIDValueMapProducer.esReducedRecHitCollection=cms.InputTag("reducedEgamma","reducedESRecHits")
-process.electronIDValueMapProducer.src=cms.InputTag("slimmedElectrons")
-process.electronIDValueMapProducer.datasetFormat=cms.string("PAT")
-
-
-process.idDemo = cms.EDAnalyzer('IDTester',
-     electrons = cms.InputTag("slimmedElectrons"),
-     heepIdMap = cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV51-miniAOD"),
-)
 
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string('electron_ntuple_mini.root')
                                    )
 
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("Geometry.CaloEventSetup.CaloTowerConstituents_cfi")
-process.load("SHarper.HEEPAnalyzer.HEEPAnalyzer_cfi")
-from SHarper.HEEPAnalyzer.HEEPAnalyzer_cfi import swapHEEPToMiniAOD
-swapHEEPToMiniAOD(process.heepAnalyzer)
 
-process.p = cms.Path(process.electronIDValueMapProducer * process.egmGsfElectronIDSequence * process.heepAnalyzer)
+#setting up the producer to make the HEEP ID value map
+from SHarper.HEEPAnalyzer.HEEPSelectionCuts_cfi import *
+process.heepId = cms.EDProducer("HEEPIdValueMapProducer",
+                                eleLabel = cms.InputTag("gedGsfElectrons"),
+                                barrelCuts = cms.PSet(heepBarrelCuts),
+                                endcapCuts = cms.PSet(heepEndcapCuts),
+                                eleIsolEffectiveAreas = cms.PSet(heepEffectiveAreas),
+                                eleRhoCorrLabel = cms.InputTag("fixedGridRhoFastjetAll"),
+                                applyRhoCorrToEleIsol = cms.bool(True),
+                                verticesLabel = cms.InputTag("offlinePrimaryVertices"),
+                                writeIdAsInt =cms.bool(True) #true saves the heep ID as an int, false: saves as a float, user request
+                                )
+process.heepIdVIDComp = cms.EDAnalyzer("HEEPIdAndVIDComp",
+                                       eleLabel=cms.InputTag("gedGsfElectrons"),
+                                       heepId=cms.InputTag("heepId"),
+                                       vid=cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV61"),
+                                       rho = cms.InputTag("fixedGridRhoFastjetAll")
+                                       )
+if useAOD==False:
+    process.heepId.verticesLabel=cms.InputTag("offlineSlimmedPrimaryVertices")
+    process.heepId.eleLabel=cms.InputTag("slimmedElectrons")
+    process.heepIdVIDComp.eleLabel=cms.InputTag("slimmedElectrons")
+ 
+
+
+process.p = cms.Path(process.heepId* #makes the HEEPID value map
+                     process.egmGsfElectronIDSequence* #makes the VID value maps
+                     process.heepIdVIDComp
+                     )
