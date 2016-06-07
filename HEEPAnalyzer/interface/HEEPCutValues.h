34fe43003af8e06e8e05b4c99d9bd42fd1490234
Sam Harper <sam.j.harper@gmail.com>
2016-06-07 20:07:21 +0100

    initial (incomplete) migration to new cut system

--- HEEPAnalyzer/interface/HEEPCutValues.h
+++ HEEPAnalyzer/interface/HEEPCutValues.h
@@ -8,6 +8,10 @@
 #include <iostream>
 #include <string>
 
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "SHarper/HEEPAnalyzer/interface/HEEPGsfEleExtra.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+
 namespace edm{
   class ParameterSet;
 }
@@ -31,21 +35,116 @@ namespace heep {
     double isolEmHadDepth1ConstTerm;
     double isolEmHadDepth1GradTerm;
     double isolEmHadDepth1GradStart;
+    double isolEmHadDepth1RhoEA;
     double isolHadDepth2ConstTerm;
     double isolHadDepth2GradTerm;
     double isolHadDepth2GradStart;
+    double isolHadDepth2RhoEA;
     double isolPtTrksConstTerm;
     double isolPtTrksGradTerm;
     double isolPtTrksGradStart;
+    double isolPtTrksRhoStart; //new for HEPE V6.1
+    double isolPtTrksRhoEA;
     double maxIsolPtTrksRel03; // WP80
+    double isolPtTrksRelRhoEA;
     double maxIsolEmRel03;     // WP80
+    double isolEmRelRhoEA;
     double maxIsolHadRel03;    // WP80
+    double isolHadRelRhoEA;
     int maxNrMissHits; 
     float maxDXY; //HEEP V4.1 conversion cut
     
     EleCutValues();
     explicit EleCutValues(const edm::ParameterSet& iConfig);
     EleCutValues& operator=(const edm::ParameterSet& iConfig);
+    
+    float getEt(const reco::GsfElectron& ele)const{return ele.et();}
+      
+    bool passEt(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      return getEt(ele)>=minEt;
+    }
+    bool passDetEta(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float etaAbs = std::abs(ele.superCluster()->eta());
+      return etaAbs>= minEta && etaAbs<=maxEta;
+    }
+    bool passCrack(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      return !ele.isEBEtaGap();
+    }
+    bool passEcalDriven(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      return ele.ecalDrivenSeed();
+    }
+    bool passDEtaIn(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float dEtaInAbs = std::abs(ele.deltaEtaSuperClusterTrackAtVtx());
+      return dEtaInAbs <= std::max(dEtaInConstTerm+dEtaInGradTerm*getEt(ele),maxDEtaIn);
+    }
+    bool passDEtaInSeed(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float dEtaInAbs = std::abs(ele.deltaEtaSeedClusterTrackAtVtx());
+      return dEtaInAbs <= maxDEtaIn;
+    }
+    bool passDPhiIn(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float dPhiInAbs = std::abs(ele.deltaPhiSuperClusterTrackAtVtx());
+      return dPhiInAbs <= maxDPhiIn;
+    }
+    bool passHadem(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float scEnergy = ele.superCluster()->energy();
+      return ele.hadronicOverEm()*scEnergy <= hademConstTerm + maxHadem*scEnergy;
+    }
+    bool passSigmaIEtaIEta(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      return ele.full5x5_sigmaIetaIeta()<maxSigmaIEtaIEta;
+    }
+    bool passIsolEmHadDepth1(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float isol = ele.dr03EcalRecHitSumEt()+ele.dr03HcalDepth1TowerSumEt();
+      const float isolRhoCorr = isol - eleExtra.rho*isolEmHadDepth1RhoEA;
+      const float etFromStart =  std::max(0.,getEt(ele)-isolEmHadDepth1GradStart);
+      const float cutValue = isolEmHadDepth1ConstTerm + isolEmHadDepth1GradTerm*etFromStart;
+      return isolRhoCorr <= cutValue;
+      
+    }
+    bool passE2x5Over5x5(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      return ele.full5x5_e2x5Max()/ele.full5x5_e5x5() >= minE2x5Over5x5 ||
+	ele.full5x5_e1x5()/ele.full5x5_e5x5()<minE1x5Over5x5;
+    }
+    bool passIsolHadDepth2(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float isol = ele.dr03HcalDepth2TowerSumEt();
+      const float isolRhoCorr = isol - eleExtra.rho*isolHadDepth2RhoEA;
+      const float etFromStart =  std::max(0.,getEt(ele)-isolHadDepth2GradStart);
+      const float cutValue = isolHadDepth2ConstTerm + isolHadDepth2GradTerm*etFromStart;
+      return isolRhoCorr <= cutValue;
+    }
+    bool passIsolPtTrks(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float isol = ele.dr03TkSumPt();
+      const float isolRhoCorr = getEt(ele) > isolPtTrksRhoStart ? isol - eleExtra.rho*isolPtTrksRhoEA : isol;
+      const float etFromStart =  std::max(0.,getEt(ele)-isolPtTrksGradStart);
+      const float cutValue = isolPtTrksConstTerm + isolPtTrksGradTerm*etFromStart;
+      return isolRhoCorr <= cutValue;
+    }
+    bool passIsolPtTrksRel03(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float isol = ele.dr03TkSumPt();
+      const float isolRhoCorr = isol - eleExtra.rho*isolPtTrksRelRhoEA;
+      const float relIso = isolRhoCorr/ele.trackMomentumAtVtx().rho();
+      return relIso <= maxIsolPtTrksRel03;
+    }
+    bool passIsolEmRel03(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float isol = ele.dr03EcalRecHitSumEt();
+      const float isolRhoCorr = isol - eleExtra.rho*isolEmRelRhoEA;
+      const float relIso = isolRhoCorr/ele.trackMomentumAtVtx().rho();
+      return relIso <= maxIsolEmRel03;
+    }
+    bool passIsolHadRel03(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float isol = ele.dr03HcalTowerSumEt();
+      const float isolRhoCorr = isol - eleExtra.rho*isolHadRelRhoEA;
+      const float relIso = isolRhoCorr/ele.trackMomentumAtVtx().rho();
+      return relIso <= maxIsolHadRel03;
+    }
+    bool passNrMissHits(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      return ele.gsfTrack()->hitPattern().numberOfHits(reco::HitPattern::MISSING_INNER_HITS) <= maxNrMissHits;
+    }
+    bool passDXY(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra)const{
+      const float dxyAbs = std::abs(ele.gsfTrack()->dxy(eleExtra.vertex));
+      return dxyAbs<=maxDXY;
+    }
+
+
 
   };
 }
--- HEEPAnalyzer/src/HEEPEleSelector.cc
+++ HEEPAnalyzer/src/HEEPEleSelector.cc
@@ -11,7 +11,6 @@ void heep::EleSelector::setup(const edm::ParameterSet& iConfig)
 {
   barrelCutValues_ = iConfig.getParameter<edm::ParameterSet>("barrelCuts");
   endcapCutValues_ = iConfig.getParameter<edm::ParameterSet>("endcapCuts");
-  isolEffectAreas_ = iConfig.getParameter<edm::ParameterSet>("eleIsolEffectiveAreas");
 }
 
 
@@ -22,79 +21,41 @@ int heep::EleSelector::getCutCode(const heep::Ele& ele,const int cutMask)const
   else return CutCodes::INVALID;
 }
 
-//if it fails a cut, set the bit
-//use the enums for speed
-int heep::EleSelector::getCutCode(const heep::Ele& ele,const EleCutValues& cuts,const int cutMask)
-{ 
-  int cutCode = 0x0;
-  if(ele.et()< cuts.minEt) cutCode |= CutCodes::ET;
-  if(ele.detEtaAbs()< cuts.minEta || ele.detEtaAbs()>cuts.maxEta) cutCode |= CutCodes::DETETA;
-  if(ele.gsfEle().isEBEtaGap()) cutCode |= CutCodes::CRACK;
-  if(!ele.isEcalDriven()) cutCode |= CutCodes::ECALDRIVEN;
-  if(fabs(ele.dEtaIn()) > std::max(cuts.dEtaInConstTerm+cuts.dEtaInGradTerm*ele.et(),cuts.maxDEtaIn )) cutCode |=CutCodes::DETAIN;
-  if(fabs(ele.dEtaInSeed()) > cuts.maxDEtaIn)   cutCode |=CutCodes::DETAINSEED;
-  if(fabs(ele.dPhiIn()) > cuts.maxDPhiIn ) cutCode |=CutCodes::DPHIIN;
-  if(ele.hOverE()*ele.superCluster().energy()> cuts.hademConstTerm + cuts.maxHadem*ele.superCluster().energy()) cutCode |= CutCodes::HADEM;
-  if(ele.sigmaIEtaIEtaFull5x5()>cuts.maxSigmaIEtaIEta) cutCode |= CutCodes::SIGMAIETAIETA;
-  if(ele.isolEmHadDepth1()>( cuts.isolEmHadDepth1ConstTerm + cuts.isolEmHadDepth1GradTerm*(ele.et()<cuts.isolEmHadDepth1GradStart ? 0. : (ele.et()-cuts.isolEmHadDepth1GradStart)))) cutCode |=CutCodes::ISOLEMHADDEPTH1;
-  if(ele.e2x5MaxOver5x5Full5x5()< cuts.minE2x5Over5x5 && ele.e1x5Over5x5Full5x5()<cuts.minE1x5Over5x5) cutCode |=CutCodes::E2X5OVER5X5;
-  if(ele.isolHadDepth2()> (cuts.isolHadDepth2ConstTerm + cuts.isolHadDepth2GradTerm*(ele.et()<cuts.isolHadDepth2GradStart ? 0. : (ele.et()-cuts.isolHadDepth2GradStart)))) cutCode |=CutCodes::ISOLHADDEPTH2;
-  if(ele.isolPtTrks() > (cuts.isolPtTrksConstTerm + cuts.isolPtTrksGradTerm*(ele.et()<cuts.isolPtTrksGradStart ? 0. : (ele.et()-cuts.isolPtTrksGradStart))))cutCode |=CutCodes::ISOLPTTRKS;
-  if(ele.isolPtTrksRel03() > cuts.maxIsolPtTrksRel03) cutCode |=CutCodes::ISOLPTTRKSREL03; 
-  if(ele.isolEmRel03() > cuts.maxIsolEmRel03 ) cutCode |=CutCodes::ISOLEMREL03; 
-  if(ele.isolHadRel03() > cuts.maxIsolHadRel03 ) cutCode |=CutCodes::ISOLHADREL03; 
-  if(ele.nrMissHits() > cuts.maxNrMissHits ) cutCode |=CutCodes::NRMISSHITS; 
-  if(fabs(ele.dxy()) > cuts.maxDXY) cutCode |=CutCodes::DXY;
-  return (cutCode & cuts.cutMask & cutMask) ;
-}
 
-
-int heep::EleSelector::getCutCode(const float rho,const math::XYZPoint &vertex,const reco::GsfElectron& ele,const int cutMask)const
+int heep::EleSelector::getCutCode(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra,
+				  const int cutMask)const
 {
   const EleCutValues* cuts = ele.isEB() ? getBarrelCuts() : getEndcapCuts();
-  if(cuts!=NULL) return getCutCode(rho,vertex,ele,isolEffectAreas_,*cuts,cutMask);
+  if(cuts!=NULL) return getCutCode(ele,eleExtra,*cuts,cutMask);
   else return CutCodes::INVALID;
 }
 
-int heep::EleSelector::getCutCode(const float rho,const std::vector<reco::Vertex>& vertices,const reco::GsfElectron& ele,const int cutMask)const
-{
-  math::XYZPoint vertex(0,0,0);
-  if(!vertices.empty()) vertex = vertices[0].position();
-  return getCutCode(rho,vertex,ele,cutMask);
-
-}
 
 
 
 //if it fails a cut, set the bit    
 //use the enums for speed
-int heep::EleSelector::getCutCode(const float rho,const math::XYZPoint &vertex,const reco::GsfElectron& ele,const heep::EffectiveAreas& effectAreas,const EleCutValues& cuts,const int cutMask)
+int heep::EleSelector::getCutCode(const reco::GsfElectron& ele,const heep::GsfEleExtra& eleExtra,const EleCutValues& cuts,const int cutMask)
 { 
   int cutCode = 0x0;
-  //now we need to calculate the et of the gsf electron using supercluster energy
-  //as GsfElectron by default uses weighted combination of tracker+supercluster or in certain situations just tracker
-  float et = ele.energy()!=0. ? ele.et()/ele.energy()*ele.caloEnergy() : 0.;
-  float scEnergy = ele.superCluster()->energy();
-  et = ele.et();
-  
-  if(et< cuts.minEt) cutCode |= CutCodes::ET;
-  if(fabs(ele.superCluster()->eta())< cuts.minEta || fabs(ele.superCluster()->eta())>cuts.maxEta) cutCode |= CutCodes::DETETA;
-  if(ele.isEBEtaGap()) cutCode |= CutCodes::CRACK;
-  if(!ele.ecalDrivenSeed()) cutCode |= CutCodes::ECALDRIVEN;
-  if(fabs(ele.deltaEtaSuperClusterTrackAtVtx()) > std::max(cuts.dEtaInConstTerm+cuts.dEtaInGradTerm*ele.et(),cuts.maxDEtaIn )) cutCode |=CutCodes::DETAIN;
-  if(fabs(heep::Ele::dEtaInSeed(ele)) > cuts.maxDEtaIn)   cutCode |=CutCodes::DETAINSEED;
-  if(fabs(ele.deltaPhiSuperClusterTrackAtVtx()) > cuts.maxDPhiIn ) cutCode |=CutCodes::DPHIIN;
-  if(ele.hadronicOverEm()*scEnergy > cuts.hademConstTerm + cuts.maxHadem*scEnergy) cutCode |= CutCodes::HADEM;
-  if(ele.full5x5_sigmaIetaIeta()>cuts.maxSigmaIEtaIEta) cutCode |= CutCodes::SIGMAIETAIETA;
-  if(ele.dr03EcalRecHitSumEt()+ele.dr03HcalDepth1TowerSumEt() - rho*(effectAreas.ecal(ele.superCluster()->eta())+effectAreas.hcal(ele.superCluster()->eta())) >( cuts.isolEmHadDepth1ConstTerm + cuts.isolEmHadDepth1GradTerm*(et<cuts.isolEmHadDepth1GradStart ? 0. : (et-cuts.isolEmHadDepth1GradStart)))) cutCode |=CutCodes::ISOLEMHADDEPTH1; 
-  if(ele.full5x5_e2x5Max()/ele.full5x5_e5x5()< cuts.minE2x5Over5x5 && ele.full5x5_e1x5()/ele.full5x5_e5x5()<cuts.minE1x5Over5x5) cutCode |=CutCodes::E2X5OVER5X5;
-  if(ele.dr03HcalDepth2TowerSumEt() - rho*effectAreas.hcal(ele.superCluster()->eta()) > (cuts.isolHadDepth2ConstTerm + cuts.isolHadDepth2GradTerm*(et<cuts.isolHadDepth2GradStart ? 0. : (et-cuts.isolHadDepth2GradStart)))) cutCode |=CutCodes::ISOLHADDEPTH2;
-  if(ele.dr03TkSumPt() - rho*effectAreas.tracker(ele.superCluster()->eta()) > (cuts.isolPtTrksConstTerm + cuts.isolPtTrksGradTerm*(et<cuts.isolPtTrksGradStart ? 0. : (et-cuts.isolPtTrksGradStart))))cutCode |=CutCodes::ISOLPTTRKS;
-  if((ele.dr03TkSumPt()- rho*effectAreas.tracker(ele.superCluster()->eta()))/ele.trackMomentumAtVtx().rho() > cuts.maxIsolPtTrksRel03) cutCode |=CutCodes::ISOLPTTRKSREL03; 
-  if((ele.dr03EcalRecHitSumEt()- rho*effectAreas.ecal(ele.superCluster()->eta()))/ele.trackMomentumAtVtx().rho() > cuts.maxIsolEmRel03 ) cutCode |=CutCodes::ISOLEMREL03; 
-  if((ele.dr03HcalTowerSumEt()- rho*effectAreas.hcal(ele.superCluster()->eta()))/ele.trackMomentumAtVtx().rho()  > cuts.maxIsolHadRel03 ) cutCode |=CutCodes::ISOLHADREL03;   
-  if(ele.gsfTrack()->hitPattern().numberOfHits(reco::HitPattern::MISSING_INNER_HITS) > cuts.maxNrMissHits ) cutCode |=CutCodes::NRMISSHITS; 
-  if(fabs(ele.gsfTrack()->dxy(vertex))>cuts.maxDXY) cutCode |=CutCodes::DXY;
+  if(!cuts.passEt(ele,eleExtra)) cutCode |= CutCodes::ET;
+  if(!cuts.passDetEta(ele,eleExtra)) cutCode |=CutCodes::DETETA;
+  if(!cuts.passCrack(ele,eleExtra)) cutCode |=CutCodes::CRACK;
+  if(!cuts.passEcalDriven(ele,eleExtra)) cutCode |= CutCodes::ECALDRIVEN;
+  if(!cuts.passDEtaIn(ele,eleExtra)) cutCode |=CutCodes::DETAIN;
+  if(!cuts.passDEtaInSeed(ele,eleExtra))   cutCode |=CutCodes::DETAINSEED;
+  if(!cuts.passDPhiIn(ele,eleExtra)) cutCode |=CutCodes::DPHIIN;
+  if(!cuts.passHadem(ele,eleExtra)) cutCode |= CutCodes::HADEM;
+  if(!cuts.passSigmaIEtaIEta(ele,eleExtra)) cutCode |= CutCodes::SIGMAIETAIETA;
+  if(!cuts.passIsolEmHadDepth1(ele,eleExtra)) cutCode |=CutCodes::ISOLEMHADDEPTH1; 
+  if(!cuts.passE2x5Over5x5(ele,eleExtra)) cutCode |=CutCodes::E2X5OVER5X5;
+  if(!cuts.passIsolHadDepth2(ele,eleExtra)) cutCode |=CutCodes::ISOLHADDEPTH2;
+  if(!cuts.passIsolPtTrks(ele,eleExtra)) cutCode |=CutCodes::ISOLPTTRKS;
+  if(!cuts.passIsolPtTrksRel03(ele,eleExtra)) cutCode |=CutCodes::ISOLPTTRKSREL03; 
+  if(!cuts.passIsolEmRel03(ele,eleExtra)) cutCode |=CutCodes::ISOLEMREL03; 
+  if(!cuts.passIsolHadRel03(ele,eleExtra)) cutCode |=CutCodes::ISOLHADREL03;   
+  if(!cuts.passNrMissHits(ele,eleExtra)) cutCode |=CutCodes::NRMISSHITS; 
+  if(!cuts.passDXY(ele,eleExtra)) cutCode |=CutCodes::DXY;
 
   return (cutCode & cuts.cutMask & cutMask) ;
 }
