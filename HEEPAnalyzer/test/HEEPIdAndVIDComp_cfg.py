c4ee0c34cc784dbd4f46f98f770b0e78db28d4c1
Sam Harper <sam.j.harper@gmail.com>
2016-06-07 09:26:16 +0100

    setting up config for 80X

--- HEEPAnalyzer/test/HEEPIdAndVIDComp_cfg.py
+++ HEEPAnalyzer/test/HEEPIdAndVIDComp_cfg.py
@@ -12,26 +12,17 @@ process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
 
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-# NOTE: the pick the right global tag!
-#    for PHYS14 scenario PU4bx50 : global tag is ???
-#    for PHYS14 scenario PU20bx25: global tag is PHYS14_25_V1
-#  as a rule, find the global tag in the DAS under the Configs for given dataset
-process.GlobalTag.globaltag = 'MCRUN2_74_V9A'
+from Configuration.AlCa.autoCond import autoCond
+process.GlobalTag.globaltag = autoCond['run2_mc'] 
+#process.GlobalTag.globaltag = autoCond['run2_data']
 
 #
 # Define input data to read
 #
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring(
-   #
-   # Just a handful of files from the dataset are listed below, for testing
-   #
-    #   '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/0432E62A-7A6C-E411-87BB-002590DB92A8.root',
-    #  '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/06C61714-7E6C-E411-9205-002590DB92A8.root',
-    #   '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/0EAD09A8-7C6C-E411-B903-0025901D493E.root'
-    '/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/Asympt50ns_MCRUN2_74_V9A-v2/00000/0AFC520A-8607-E511-B099-001517FB20EC.root',
-'/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/Asympt50ns_MCRUN2_74_V9A-v2/00000/0C4979A5-9D07-E511-9551-0025905938D4.root',
-'/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/Asympt50ns_MCRUN2_74_V9A-v2/00000/0C4DF708-7C07-E511-A290-00259073E3AE.root',
+  'file:/home/ppd/mjf21517/scratch/mcTestFiles/ZToEE_NNPDF30_13TeV-powheg_M_200_400_PUSpring16_80X_mcRun2_asymptotic_2016_v3-v2_FE668C0A-8B09-E611-ACDE-B083FED76DBD.root',
+  'file:/home/ppd/mjf21517/scratch/mcTestFiles/ZToEE_NNPDF30_13TeV-powheg_M_6000_Inf_PUSpring16_80X_mcRun2_asymptotic_2016_v3-v2_6240D9B6-350A-E611-9661-A0369F7FC714.root',
 ),
               #               eventsToProcess = cms.untracked.VEventRange("1:2862883-1:2862883")
                              
@@ -64,8 +55,7 @@ else :
 switchOnVIDElectronIdProducer(process, dataFormat)
 
 # define which IDs we want to produce
-my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_PHYS14_PU20bx25_V2_cff',
-                 'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV60_cff']
+my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV60_80XAOD_cff']
 
 #add them to the VID producer
 for idmod in my_id_modules:
@@ -75,7 +65,7 @@ for idmod in my_id_modules:
 process.heepIdVIDComp = cms.EDAnalyzer("HEEPIdAndVIDComp",
                                        eleLabel=cms.InputTag("gedGsfElectrons"),
                                        heepId=cms.InputTag("heepId"),
-                                       vid=cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV60")
+                                       vid=cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV60-80XAOD")
                                        )
 
 
@@ -85,10 +75,27 @@ if useAOD==False:
     process.heepIdVIDComp.eleLabel=cms.InputTag("slimmedElectrons")
     
 
-
-
+process.heepIDVarValueMaps = cms.EDProducer("ElectronHEEPIDValueMapProducer",
+                                            ebRecHits=cms.InputTag("reducedEcalRecHitsEB"),
+                                            eeRecHits=cms.InputTag("reducedEcalRecHitsEB"),
+                                            beamSpot=cms.InputTag("offlineBeamSpot"),
+                                            tracks=cms.InputTag("generalTracks"),
+                                            eles=cms.InputTag("gedGsfElectrons"),
+                                            trkIsoConfig= cms.PSet(
+                                               extRadius=cms.double(0.3),
+                                               intRadiusBarrel=cms.double(0.015),
+                                               intRadiusEndcap=cms.double(0.015),
+                                               stripBarrel=cms.double(0.015),
+                                               stripEndcap=cms.double(0.015),
+                                               ptMin=cms.double(0.7),
+                                               maxVtxDist=cms.double(0.2),
+                                               drb=cms.double(999999999.0)
+                                               )
+                                            )
+                                            
 
 process.p = cms.Path(process.heepId* #makes the HEEPID value map
+                     process.heepIDVarValueMaps*
                      process.egmGsfElectronIDSequence* #makes the VID value maps
                      process.heepIdVIDComp
                      )
