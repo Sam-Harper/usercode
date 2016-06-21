e581c7980f7f7a26c5168576ef76a47e7def590d
Sam Harper <sam.j.harper@gmail.com>
2016-06-21 17:04:26 +0100

    updating an example

--- HEEPAnalyzer/test/HEEPIdValueMap_cfg.py
+++ HEEPAnalyzer/test/HEEPIdValueMap_cfg.py
@@ -12,46 +12,52 @@ process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
 
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-# NOTE: the pick the right global tag!
-#    for PHYS14 scenario PU4bx50 : global tag is ???
-#    for PHYS14 scenario PU20bx25: global tag is PHYS14_25_V1
-#  as a rule, find the global tag in the DAS under the Configs for given dataset
-process.GlobalTag.globaltag = 'PHYS14_25_V1'
+from Configuration.AlCa.autoCond import autoCond
+process.GlobalTag.globaltag = autoCond['run2_mc'] 
+
 
 #
 # Define input data to read
 #
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring(
-   #
-   # Just a handful of files from the dataset are listed below, for testing
-   #
-       '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/0432E62A-7A6C-E411-87BB-002590DB92A8.root',
-      '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/06C61714-7E6C-E411-9205-002590DB92A8.root',
-       '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/0EAD09A8-7C6C-E411-B903-0025901D493E.root'
- ),
-              #               eventsToProcess = cms.untracked.VEventRange("1:2862883-1:2862883")
+  'file:/home/ppd/mjf21517/scratch/mcTestFiles/ZToEE_NNPDF30_13TeV-powheg_M_200_400_PUSpring16_80X_mcRun2_asymptotic_2016_v3-v2_FE668C0A-8B09-E611-ACDE-B083FED76DBD.root',
+  '/store/mc/RunIISpring16DR80/ZToEE_NNPDF30_13TeV-powheg_M_6000_Inf/AODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/50000/0015C81C-370A-E611-8073-0CC47A01CAFC.root',
+),
+                             eventsToProcess = cms.untracked.VEventRange("1:2862883-1:2862883")
                              
 )
-
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("output.root")
+)
 
 #setting up the producer to make the HEEP ID value map
 from SHarper.HEEPAnalyzer.HEEPSelectionCuts_cfi import *
 process.HEEPId = cms.EDProducer("HEEPIdValueMapProducer",
                                 eleLabel = cms.InputTag("gedGsfElectrons"),
-                                barrelCuts = cms.PSet(heepBarrelCuts),
-                                endcapCuts = cms.PSet(heepEndcapCuts),
-                                eleIsolEffectiveAreas = cms.PSet(heepEffectiveAreas),
-                                eleRhoCorrLabel = cms.InputTag("fixedGridRhoFastjetAll"),
-                                applyRhoCorrToEleIsol = cms.bool(True),
-                                verticesLabel = cms.InputTag("offlinePrimaryVertices"),
+                                barrelCuts = cms.PSet(heepBarrelCuts), #for V6.1 (miniAOD compromise) do heeepBarrelCutsV61
+                                endcapCuts = cms.PSet(heepEndcapCuts) ,#for V6.1 (miniAOD compromise) do heeepBarrelCutsV61
+                                trkIsoNoJetCoreTag = cms.InputTag(""),
+                                nrSatCrysIn5x5Tag = cms.InputTag(""),
+                                eleRhoCorrTag = cms.InputTag("fixedGridRhoFastjetAll"),
+                                verticesTag = cms.InputTag("offlinePrimaryVertices"),
+                                beamSpotTag = cms.InputTag("offlineBeamSpot"),
+                                reducedBarrelRecHitTag = cms.InputTag("reducedEcalRecHitsEB"),
+                                reducedEndcapRecHitTag = cms.InputTag("reducedEcalRecHitsEE"),
+                                ctfTrackTag=cms.InputTag("generalTracks"),
+                                trkIsoConfig= cms.PSet(
+                                   extRadius=cms.double(0.3),
+                                   intRadiusBarrel=cms.double(0.015),
+                                   intRadiusEndcap=cms.double(0.015),
+                                   stripBarrel=cms.double(0.015),
+                                   stripEndcap=cms.double(0.015),
+                                   ptMin=cms.double(0.7),
+                                   maxVtxDist=cms.double(0.2),
+                                   drb=cms.double(999999999.0)
+                                ),
                                 writeIdAsInt =cms.bool(True) #true saves the heep ID as an int, false: saves as a float, user request
                                 )
 
-miniAOD=True
-if miniAOD:
-    process.HEEPId.verticesLabel=cms.InputTag("offlineSlimmedPrimaryVertices")
-    process.HEEPId.eleLabel=cms.InputTag("slimmedElectrons")
 
 
 process.TFileService = cms.Service("TFileService",
