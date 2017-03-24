da0ed7380c12d109e39d31bcf09c5b691fe753af
Sam Harper <sam.j.harper@gmail.com>
2017-03-24 10:04:54 +0000

    updating hlt config

--- SHNtupliser/test/shNtupliser_runII_HLT.py
+++ SHNtupliser/test/shNtupliser_runII_HLT.py
@@ -19,7 +19,7 @@ else:
     addInputFiles(process.source,sys.argv[2:len(sys.argv)-1])
     from SHarper.SHNtupliser.datasetCodes import getDatasetCode
     datasetCode=getDatasetCode(process.source.fileNames[0])
-    datasetCode=0
+#    datasetCode=100
 
 if datasetCode==0: isMC=False
 else: isMC=True
@@ -84,14 +84,8 @@ process.shNtupliser.outputGeom = cms.bool(False)
 process.shNtupliser.hltProcName = cms.string(hltName)
 process.shNtupliser.trigEventTag = cms.InputTag("hltTriggerSummaryAOD","",hltName)
 process.shNtupliser.trigResultsTag = cms.InputTag("TriggerResults","",hltName)
-
-process.shNtupliser.electronTag = cms.untracked.InputTag("patElectrons"+patCandID)
-process.shNtupliser.tauTag = cms.untracked.InputTag("patTaus"+patCandID)
-process.shNtupliser.muonTag = cms.untracked.InputTag("patMuons"+patCandID)
-process.shNtupliser.jetTag = cms.untracked.InputTag("patJets"+patCandID)
-process.shNtupliser.photonTag = cms.untracked.InputTag("patPhotons"+patCandID)
-process.shNtupliser.metTag = cms.untracked.InputTag("patMETs"+patCandID)
 process.shNtupliser.hbheRecHitsTag = cms.InputTag("reducedHcalRecHits","hbhereco")
+process.shNtupliser.addTrigSum = cms.bool(True)
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string("output.root")
 )
@@ -112,66 +106,12 @@ else:
     #process.shNtupliser.outputFilename= sys.argv[len(sys.argv)-1]
     process.shNtupliser.datasetCode = datasetCode
     process.shNtupliser.sampleWeight = 1
-  #  print "datset code ",process.shNtupliser.datasetCode
 
-# Additional output definition
-import HLTrigger.HLTfilters.hltHighLevel_cfi
-process.skimHLTFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
-process.skimHLTFilter.throw=cms.bool(False)
-datasetName="TOSED:DATASETNAME"
 
-if datasetName=="DoubleEG":
-    print "setting up HLT skim for DoubleEG"
-    process.skimHLTFilter.HLTPaths = cms.vstring("HLT_DoubleEle33*","HLT_DoubleEle37*","HLT_DoublePhoton60_v*","HLT_DoublePhoton85_v*","HLT_ECALHT800_v*","HLT_Ele23_Ele12_CaloIdL_TrackIdL*")
-elif datasetName=="SingleElectron":
-    print "setting up HLT skim for SingleElectron"
-    process.skimHLTFilter.HLTPaths = cms.vstring("HLT_Ele105_CaloIdVT_GsfTrkIdT_v*","HLT_Ele115_CaloIdVT_GsfTrkIdT_v*","HLT_Ele27_WPLoose_Gsf_v*","HLT_Ele27_eta2p1_WPLoose_Gsf_v*","HLT_Ele27_WPTight_Gsf_v*","HLT_Ele27_eta2p1_WPTight_Gsf_v*","HLT_Ele32_eta2p1_WPTight_Gsf_v*","HLT_Ele35_WPLoose_Gsf_v*")
-elif datasetName=="SinglePhoton":
-    print "setting up HLT skim for SinglePhoton"
-    process.skimHLTFilter.HLTPaths =cms.vstring("HLT_Photon22_v*","HLT_Photon30_v*","HLT_Photon36_v*","HLT_Photon50_v*","HLT_Photon75_v*","HLT_Photon90_v*","HLT_Photon120_v*","HLT_Photon165_HE10_v*","HLT_Photon175_v*","HLT_Photon250_NoHE_v*","HLT_Photon300_NoHE_v*")
-else:
-    print "setting HLT skim to select all"
-    process.skimHLTFilter.HLTPaths = cms.vstring("HLT_*")
 
-process.egammaFilter = cms.EDFilter("EGammaFilter",
-                                    nrElesRequired=cms.int32(-1),
-                                    nrPhosRequired=cms.int32(-1),
-                                    nrSCsRequired=cms.int32(-1),
-                                    eleEtCut=cms.double(20),
-                                    phoEtCut=cms.double(20),
-                                    scEtCut=cms.double(-1),
-                                    eleTag=process.shNtupliser.gsfEleTag,
-                                    phoTag=process.shNtupliser.recoPhoTag,
-                                    superClusEBTag = cms.InputTag("particleFlowSuperClusterECAL","particleFlowSuperClusterECALBarrel"),
-                                    superClusEETag = cms.InputTag("particleFlowSuperClusterECAL","particleFlowSuperClusterECALEndcapWithPreshower"),
-                                    caloTowerTag = cms.InputTag("towerMaker"),
-                                    genEvtInfoTag=cms.InputTag("generator"),
-                                    requireEcalDriven=cms.bool(True)
-                                     )
 
-print "dataset code: ",process.shNtupliser.datasetCode.value()
-if process.shNtupliser.datasetCode.value()>=800 and process.shNtupliser.datasetCode.value()<700:
-    print "applying filter for 1 ele and disabling large collections"
-    process.egammaFilter.nrElesRequired=cms.int32(1)
-    process.shNtupliser.nrGenPartToStore = cms.int32(0)
 
-if process.shNtupliser.datasetCode.value()>140 and process.shNtupliser.datasetCode.value()<1000:
-    print "applying filter for 1 ele and disabling large collections"
-    process.egammaFilter.nrElesRequired=cms.int32(1)
-    process.shNtupliser.nrGenPartToStore = cms.int32(0)
-    process.shNtupliser.addPFCands = False
-    process.shNtupliser.addPFClusters = False
-    process.shNtupliser.addIsolTrks = False
-
-if process.shNtupliser.datasetCode.value()>10:
-    process.shNtupliser.addTrigSum = cms.bool(False)
-
-process.p = cms.Path(#process.primaryVertexFilter*
-    process.egammaFilter*
-    process.shNtupliser)
-        
-if not isMC:
-    process.p.insert(0,process.skimHLTFilter)
+process.p = cms.Path(process.shNtupliser)
 
 #from SHarper.HEEPAnalyzer.HEEPAnalyzer_cfi import swapHEEPToMiniAOD
 #swapHEEPToMiniAOD(process.shNtupliser)
