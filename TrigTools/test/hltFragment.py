5c28360b1b43066f246b85b97433fb724a0f88ae
Sam Harper <sam.j.harper@gmail.com>
2017-03-29 20:44:54 +0100

    updating fragment

--- TrigTools/test/hltFragment.py
+++ TrigTools/test/hltFragment.py
@@ -2,27 +2,20 @@
 from SHarper.TrigTools.hltConfigTools import *
 process.maxEvents.input=-1
 rmAllEndPathsWithOutput(process)
-rmPaths(process,["HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW_v3","HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v3"])
-setSaveTags(process,"HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW_v3",True)
-setSaveTags(process,"HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v3",True)
 addOutputMod(process)
 
 import FWCore.ParameterSet.VarParsing as VarParsing
 options = VarParsing.VarParsing ('analysis')
 options.parseArguments()
 
-isCrabJob=False #script seds this if its a crab job
-#if 1, its a crab job...
-if isCrabJob:
-    process.hltOutputTot.fileName=cms.untracked.string("OUTPUTFILE")
-else:
-    process.hltOutputTot.fileName=cms.untracked.string(options.outputFile)
+process.hltOutputTot.fileName=cms.untracked.string(options.outputFile)
+process.MessageLogger.suppressWarning.extend(["hltEgammaGsfTracks","hltEgammaGsfTracksUnseeded"])
+print  "global tag : ",process.GlobalTag.globaltag
 
 
-
-delattr(process,"EvFDaqDirector")
-delattr(process,"FastMonitoringService")
-delattr(process,"DQMStore")
-delattr(process,"hltDQMFileSaver")
-delattr(process,"RatesMonitoring")
-delattr(process,"MessageLogger")
+#delattr(process,"EvFDaqDirector")
+#delattr(process,"FastMonitoringService")
+#delattr(process,"DQMStore")
+#delattr(process,"hltDQMFileSaver")
+#delattr(process,"RatesMonitoring")
+#delattr(process,"MessageLogger")
