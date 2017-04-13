dcc0bcfdca0e1926236cc2107c49d852c0ddd9bc
Sam Harper <sam.j.harper@gmail.com>
2017-04-13 13:28:01 +0100

    crab config updates

--- SHNtupliser/test/crab_base.py
+++ SHNtupliser/test/crab_base.py
@@ -10,6 +10,8 @@ config.General.transferOutputs = True
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'shNtupliser_autoGen_cfg.py'
+config.JobType.maxMemoryMB = 3000
+config.JobType.numCores = 4
 #config.JobType.inputFiles=['ged_regression_20161208.db',]
 #TOSED:EXTRAJOBTYPEINFO
 
@@ -19,11 +21,11 @@ config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 'TOSED:UNITSPERJOB'
 config.Data.totalUnits = 'TOSED:TOTALUNITS'
-config.Data.publication = False
+config.Data.publication = True
 config.Data.publishDBS = 'phys03' 
 config.Data.outputDatasetTag = 'TOSED:PUBLISHDATANAME'
 config.Data.outLFNDirBase = 'TOSED:OUTPUTDIR'
- 
+config.Data.allowNonValidInputDataset = True
 config.section_("Site")
 config.Site.storageSite = "T2_UK_SGrid_RALPP"
 
