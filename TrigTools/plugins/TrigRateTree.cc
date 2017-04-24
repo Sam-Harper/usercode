b06ff0d690587256bbec63759c3510b82c7ccadd
Sam Harper <sam.j.harper@gmail.com>
2017-04-24 19:33:45 +0100

    add trig rate ntup maker

---
+++ TrigTools/plugins/TrigRateTree.cc
@@ -0,0 +1,150 @@
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/Run.h"
+#include "FWCore/Common/interface/TriggerNames.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/Common/interface/MergeableCounter.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h" 
+
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "TTree.h"
+#include <iostream>
+
+class TrigRateTree : public edm::EDAnalyzer {
+  edm::EDGetTokenT<edm::MergeableCounter> nrEventsRunToken_;
+  edm::EDGetTokenT<edm::MergeableCounter> nrEventsRunPostPUFilterToken_;
+  edm::EDGetTokenT<edm::TriggerResults> trigResultsToken_;
+  edm::EDGetTokenT<edm::TriggerResults> trigResultsPUFilterToken_;
+  edm::EDGetTokenT<std::vector<PileupSummaryInfo> > pileupInfoToken_;
+  int datasetCode_;
+
+  unsigned int nrEventsRunSum_;
+  unsigned int nrEventsRunPostPUFilterSum_;
+  std::string pileupFilterPath_;
+  std::vector<std::string> triggerPaths_;
+
+  static constexpr int kMaxNrPU_ = 70;
+
+  std::vector<std::array<int,kMaxNrPU_+1> > hltCounts_;
+  std::vector<std::array<int,kMaxNrPU_+1> > hltCountsPostPUFilter_;
+  
+  
+  
+public:
+  
+  TrigRateTree(const edm::ParameterSet& pset):nrEventsRunSum_(0),nrEventsRunPostPUFilterSum_(0){
+    nrEventsRunToken_ = consumes<edm::MergeableCounter,edm::InRun>(pset.getParameter<edm::InputTag>("nrEventsInRun"));
+    nrEventsRunPostPUFilterToken_ = consumes<edm::MergeableCounter,edm::InRun>(pset.getParameter<edm::InputTag>("nrEventsInRunPostPUFilter"));
+    trigResultsToken_ = consumes<edm::TriggerResults>(pset.getParameter<edm::InputTag>("triggerResults"));
+    trigResultsPUFilterToken_ = consumes<edm::TriggerResults>(pset.getParameter<edm::InputTag>("triggerResultsPUFilter"));
+    pileupInfoToken_ = consumes<std::vector<PileupSummaryInfo> >(pset.getParameter<edm::InputTag>("pileupInfo"));
+    pileupFilterPath_ = pset.getParameter<std::string>("pileupFilterPath");
+    triggerPaths_ = pset.getParameter<std::vector<std::string> >("triggerPaths");
+    datasetCode_ = pset.getParameter<int>("datasetCode");
+
+    hltCounts_.resize(triggerPaths_.size());
+    hltCountsPostPUFilter_.resize(triggerPaths_.size());
+    
+    
+  }
+
+  
+  void analyze(const edm::Event& event,const edm::EventSetup& setup) override;
+
+  void beginRun(const edm::Run& run,const edm::EventSetup& setup ) override{}
+   
+  void endRun(const edm::Run& run,const edm::EventSetup& setup ) override{
+    edm::Handle<edm::MergeableCounter> nrEventsHandle;
+    run.getByToken(nrEventsRunToken_,nrEventsHandle);
+    edm::Handle<edm::MergeableCounter> nrEventsPostPUFilterHandle;
+    run.getByToken(nrEventsRunPostPUFilterToken_,nrEventsPostPUFilterHandle);
+    nrEventsRunSum_+=nrEventsHandle->value; 
+    nrEventsRunPostPUFilterSum_+=nrEventsPostPUFilterHandle->value; 
+  }
+ 
+  void endJob()override;
+
+  bool checkPUFilter(const edm::Event& event);
+
+};
+
+
+namespace{
+  
+  template<typename T> edm::Handle<T> getHandle(const edm::Event& event,const edm::EDGetTokenT<T>& token){
+    edm::Handle<T> handle;
+    event.getByToken(token,handle);
+    return handle;
+  }
+}
+
+void TrigRateTree::analyze(const edm::Event& event,const edm::EventSetup& setup)
+{
+
+  auto trigResults = getHandle(event,trigResultsToken_);
+  auto trigNames = event.triggerNames(*trigResults);
+  auto puInfo = getHandle(event,pileupInfoToken_);
+  int nrTruePU = puInfo->empty() ? 0 : puInfo->front().getTrueNumInteractions();
+  if(nrTruePU>kMaxNrPU_){
+    throw cms::Exception("ConfigError") <<" # true pileup is "<<nrTruePU<<" and max allowed is "<<kMaxNrPU_<<std::endl;
+  }
+  bool passPUFilter = checkPUFilter(event);
+  
+  for(size_t pathNr=0;pathNr<triggerPaths_.size();pathNr++){
+    size_t pathIndex = trigNames.triggerIndex(triggerPaths_[pathNr]);
+    if(pathIndex<trigResults->size() && trigResults->accept(pathIndex)){
+      hltCounts_[pathNr][nrTruePU]++;
+      if(passPUFilter) hltCountsPostPUFilter_[pathNr][nrTruePU]++;
+    }
+  }
+	 
+}
+
+void TrigRateTree::endJob()
+{ 
+  edm::Service<TFileService> fs;
+  fs->file().cd();
+  TTree* tree = new TTree("rateTree","rateTree");
+  tree->Branch("nrTot",&nrEventsRunSum_,"nrTot/I");
+  tree->Branch("nrTotPostPUFilter",&nrEventsRunPostPUFilterSum_,"nrTotPostPUFilter/I");
+
+  std::vector<int> nrPass(triggerPaths_.size(),0);
+  std::vector<int> nrPassPostPUFilter(triggerPaths_.size(),0);
+  
+
+  for(size_t pathNr=0;pathNr<triggerPaths_.size();pathNr++){
+    const auto& pathName = triggerPaths_[pathNr];
+    nrPass[pathNr]= std::accumulate(hltCounts_[pathNr].begin(),hltCounts_[pathNr].end(),0);
+    nrPassPostPUFilter[pathNr] = std::accumulate(hltCountsPostPUFilter_[pathNr].begin(),hltCountsPostPUFilter_[pathNr].end(),0);
+
+    tree->Branch((pathName+"_pass").c_str(),&nrPass[pathNr],"nrPass/I");
+    tree->Branch((pathName+"_passPostPUFilter").c_str(),&nrPassPostPUFilter[pathNr],"nrPass/I");
+    tree->Branch((pathName+"_vsPU").c_str(),hltCounts_[pathNr].data(),("counts["+std::to_string(kMaxNrPU_+1)+"]/I").c_str());
+    tree->Branch((pathName+"_vsPUPostPUFilter").c_str(),hltCountsPostPUFilter_[pathNr].data(),("counts["+std::to_string(kMaxNrPU_+1)+"]/I").c_str());    
+  }
+  tree->Fill();
+  
+}
+
+
+bool TrigRateTree::checkPUFilter(const edm::Event& event) 
+{  
+
+  auto trigResults = getHandle(event,trigResultsPUFilterToken_);
+  auto trigNames = event.triggerNames(*trigResults);
+  
+  size_t pathIndex = trigNames.triggerIndex(pileupFilterPath_);
+  if(pathIndex<trigResults->size() && trigResults->accept(pathIndex)) return true;
+  else return false;
+}
+
+
+
+DEFINE_FWK_MODULE(TrigRateTree);
---
+++ TrigTools/test/trigRateTree.py
@@ -0,0 +1,102 @@
+isCrabJob=False #script seds this if its a crab job
+
+# Import configurations
+import FWCore.ParameterSet.Config as cms
+
+# set up process
+process = cms.Process("HEEP")
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(),
+                        #    eventsToProcess = cms.untracked.VEventRange("1:1484800-1:1484810"),
+#                            eventsToSkip = cms.untracked.VEventRange("1:1484806-1:1484806")
+                             )
+if isCrabJob:
+    datasetCode=DATASETCODE
+else:
+    import sys
+    from SHarper.SHNtupliser.addInputFiles import addInputFiles
+    addInputFiles(process.source,sys.argv[2:len(sys.argv)-1])
+    from SHarper.SHNtupliser.datasetCodes import getDatasetCode
+    datasetCode=getDatasetCode(process.source.fileNames[0])
+    datasetCode=100
+
+if datasetCode==0: isMC=False
+else: isMC=True
+
+print "isCrab = ",isCrabJob,"isMC = ",isMC," datasetCode = ",datasetCode
+
+# initialize MessageLogger and output report
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
+    reportEvery = cms.untracked.int32(10000),
+    limit = cms.untracked.int32(10000000)
+)
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+
+#Load geometry
+process.load("Configuration.Geometry.GeometryRecoDB_cff")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.autoCond import autoCond
+if isMC:
+    process.GlobalTag.globaltag = autoCond['run2_mc'] 
+else:
+    process.GlobalTag.globaltag = autoCond['run2_data']
+
+
+# set the number of events
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.load("Configuration.StandardSequences.Services_cff")
+
+
+process.trigRateTree = cms.EDAnalyzer("TrigRateTree",
+                                      nrEventsInRun=cms.InputTag("nrEventsStorer","nrEventsRun"),
+                                      nrEventsInRunPostPUFilter=cms.InputTag("nrEventsStorerPostPUFilter","nrEventsRun"),
+                                      triggerResults=cms.InputTag("TriggerResults","","HLTX"),
+                                      triggerResultsPUFilter=cms.InputTag("TriggerResults","","HLTSkim"),
+                                      pileupInfo = cms.InputTag("addPileupInfo"),
+                                      pileupFilterPath = cms.string("HLT_RemovePileUpDominatedEventsGen_v1"),
+                                      triggerPaths= cms.vstring("HLT_Ele33_CaloIdL_v1",
+                                                                "HLT_Ele27_WPTight_Gsf_v7",
+                                                                "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v9"),
+                                      datasetCode=cms.int32(datasetCode)
+                                      )
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("output.root")
+)
+#process.shNtupliser.gsfEleTag = cms.InputTag("gedGsfElectronsTrkIsoCorr")
+import os
+
+
+#if 1, its a crab job...
+if isCrabJob:
+    print "using crab specified filename"
+    process.TFileService.fileName= "OUTPUTFILE"
+    process.trigRateTree.datasetCode = datasetCode
+
+else:
+    print "using user specified filename"
+    process.TFileService.fileName= sys.argv[len(sys.argv)-1]
+    process.trigRateTree.datasetCode = datasetCode
+
+
+
+#import HLTrigger.HLTfilters.hltHighLevel_cfi
+#process.skimHLTFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+#process.skimHLTFilter.throw=cms.bool(False)
+#process.skimHLTFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLTSkim")
+#process.skimHLTFilter.HLTPaths = cms.vstring("HLT_RemovePileUpDominatedEventsGen_v1",)
+
+
+process.p = cms.Path(process.trigRateTree)
+
+#from SHarper.HEEPAnalyzer.HEEPAnalyzer_cfi import swapHEEPToMiniAOD
+#swapHEEPToMiniAOD(process.shNtupliser)
+#process.source.eventsToProcess = cms.untracked.VEventRange("1:57017-1:57017",)
+#from SHarper.HEEPAnalyzer.heepTools import *
+#swapCollection(process,"gsfElectrons","gsfElectronsHEEPCorr")
