a29d0542081511bde77c223095381e3229292751
Sam Harper <sam.j.harper@gmail.com>
2017-06-25 15:40:42 +0100

    without ps info

--- TrigTools/plugins/TrigResultTreeMakerV2.cc
+++ TrigTools/plugins/TrigResultTreeMakerV2.cc
@@ -50,8 +50,8 @@ private:
   std::vector<std::string> pathNames_;
   std::vector<std::string> pathNamesP5_;
  
-  std::unique_ptr<HLTPrescaleProvider> hltConfig_;
-  std::unique_ptr<HLTPrescaleProvider> hltConfigP5_;
+  std::unique_ptr<HLTConfigProvider> hltConfig_;
+  std::unique_ptr<HLTConfigProvider> hltConfigP5_;
  
   trigtools::EvtInfoStruct evtInfo_;
   std::vector<unsigned int> trigBits_;
@@ -88,9 +88,9 @@ TrigResultTreeMakerV2::TrigResultTreeMakerV2(const edm::ParameterSet& iPara):tre
   trigResultsP5Tag_ = iPara.getParameter<edm::InputTag>("trigResultsP5Tag");
   trigResultsP5Token_ = consumes<edm::TriggerResults>(trigResultsP5Tag_);
 
-  hltConfig_ = std::make_unique<HLTPrescaleProvider>(iPara,consumesCollector(),*this);
-  hltConfigP5_ = std::make_unique<HLTPrescaleProvider>(iPara,consumesCollector(),*this);
-
+  hltConfig_ = std::make_unique<HLTConfigProvider>();//iPara,consumesCollector(),*this);
+  hltConfigP5_ = std::make_unique<HLTConfigProvider>();//iPara,consumesCollector(),*this);
+    
   refTrigger_ = iPara.getParameter<std::string>("refTrigger");
 
 }
@@ -100,28 +100,28 @@ void TrigResultTreeMakerV2::beginRun(const edm::Run& run,const edm::EventSetup& 
   std::cout <<"begining run "<<std::endl;
   bool changed=false;
   hltConfig_->init(run,setup,trigResultsTag_.process(),changed); //as we need the orginal HLT config...
-  std::cout <<"new table name "<<hltConfig_->hltConfigProvider().tableName()<<std::endl;
+  std::cout <<"new table name "<<hltConfig_->tableName()<<std::endl;
 
   bool changedP5=false;
   hltConfigP5_->init(run,setup,trigResultsP5Tag_.process(),changed); //as we need the orginal HLT config...
-  std::cout <<"new table name "<<hltConfigP5_->hltConfigProvider().tableName()<<std::endl;
+  std::cout <<"new table name "<<hltConfigP5_->tableName()<<std::endl;
   
   
   if((changed || changedP5) && !pathNames_.empty()){ 
     std::cout <<"TrigResultTreeMakerV2::beginRun warning HLT menu has changed new: "<<changed<<" p5 "<<changedP5<<std::endl;
   }
   if(pathNames_.empty()){ //initialising hlt paths names
-    pathNames_ = hltConfig_->hltConfigProvider().triggerNames();
-    pathNamesP5_ = hltConfigP5_->hltConfigProvider().triggerNames();
+    pathNames_ = hltConfig_->triggerNames();
+    pathNamesP5_ = hltConfigP5_->triggerNames();
     
     edm::Service<TFileService> fs;
     TFile* file = &fs->file();
     file->cd();
     
-    size_t nrInts = pathNames_.size()/kWordSize;
+    size_t nrInts = pathNames_.size()/kWordSize+1;
     trigBits_.resize(nrInts,0);
 
-    size_t nrIntsP5 = pathNamesP5_.size()/kWordSize;
+    size_t nrIntsP5 = pathNamesP5_.size()/kWordSize+1;
     trigBitsP5_.resize(nrIntsP5,0);
     
     tree_=new TTree("trigResultTree","tree");
@@ -148,7 +148,7 @@ namespace {
 	  unsigned int bit = 0x1 << wordAndBitNr.second;
 	  trigBits[wordAndBitNr.first] |= bit;
 	}else{
-	  std::cout <<"Warning word number is "<<wordAndBitNr.first<<" while trigBits has "<<trigBits.size()<<" words "<<std::endl;
+	  std::cout <<"for path "<<pathNr<<" "<<pathNames[pathNr]<<" Warning word number is "<<wordAndBitNr.first<<" while trigBits has "<<trigBits.size()<<" words "<<std::endl;
 	}
       }
     }
@@ -170,8 +170,8 @@ void TrigResultTreeMakerV2::analyze(const edm::Event& iEvent, const edm::EventSe
   
   evtInfo_.fill(iEvent);
 
-  psColumn_= hltConfigP5_->prescaleSet(iEvent,iSetup);
-  hltPhysicsPS_ = hltConfigP5_->prescaleValues(iEvent,iSetup,refTrigger_).second;
+  psColumn_= -1;// hltConfigP5_->prescaleSet(iEvent,iSetup);
+  hltPhysicsPS_ =-1;// = hltConfigP5_->prescaleValues(iEvent,iSetup,refTrigger_).second;
   fillTrigInfo(pathNames_,trigNames,trigResults,trigBits_,kWordSize);
   fillTrigInfo(pathNamesP5_,trigNamesP5,trigResultsP5,trigBitsP5_,kWordSize);
 
