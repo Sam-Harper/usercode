bab66226a5bbac71d24de071bed1c512f3c644fe
Sam Harper <sam.j.harper@gmail.com>
2019-01-17 14:14:50 +0000

    adding EcalClusIsoTreeMaker

---
+++ TrigNtup/plugins/EcalClusIsoTreeMaker.cc
@@ -0,0 +1,175 @@
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
+#include "DataFormats/ParticleFlowReco/interface/PFCluster.h"
+#include "DataFormats/ParticleFlowReco/interface/PFClusterFwd.h"
+ 
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+#include "DataFormats/EcalDetId/interface/EEDetId.h"
+#include "DataFormats/Common/interface/View.h"
+#include "DataFormats/Common/interface/ValueMap.h"
+
+#include "RecoEgamma/EgammaIsolationAlgos/interface/EcalPFClusterIsolation.h"
+
+
+
+#include "TFile.h"
+#include "TTree.h"
+
+#include <string>
+
+struct EvtStruct {
+  int runnr,lumiSec,eventnr;
+  static std::string contents(){return "runnr/I:lumiSec:eventnr";}
+  void clear(){runnr=lumiSec=eventnr=0;}
+  void fill(const edm::Event& event);
+};
+
+
+struct PFEcalClusTreeStruct {
+  EvtStruct evt;
+  unsigned int seedId;
+  float eleEt;
+  float eleEta;
+  float elePhi;
+  float oldPFEcalIso;
+  float newPFEcalIso;
+
+  void createBranches(TTree* tree);
+  void setBranchAddresses(TTree* tree){}
+  void fill(const edm::Event& event,const edm::Ref<std::vector<reco::GsfElectron> > ele,const edm::ValueMap<float>& oldPfEcalIso,const edm::Handle<std::vector<reco::PFCluster> > newPFClusters);
+
+};
+
+class EcalClusIsoTreeMaker : public edm::EDAnalyzer {
+
+private:
+  PFEcalClusTreeStruct pfEcalClusData_;
+  TTree* pfEcalClusTree_;
+
+
+  edm::EDGetTokenT<edm::ValueMap<float> > pfEcalIsoToken_;
+  edm::EDGetTokenT<std::vector<reco::PFCluster> > pfEcalClusToken_;
+  edm::EDGetTokenT<std::vector<reco::GsfElectron> > elesToken_;
+
+  EcalClusIsoTreeMaker(const EcalClusIsoTreeMaker& rhs)=delete;
+  EcalClusIsoTreeMaker& operator=(const EcalClusIsoTreeMaker& rhs)=delete;
+
+public:
+  explicit EcalClusIsoTreeMaker(const edm::ParameterSet& iPara);
+  virtual ~EcalClusIsoTreeMaker();
+  
+private:
+  virtual void beginJob();
+  virtual void beginRun(const edm::Run& run,const edm::EventSetup& iSetup);
+  virtual void analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup);
+  virtual void endRun(edm::Run const& iRun, edm::EventSetup const&){}
+  virtual void endJob();
+  template<typename T>
+  void setToken(edm::EDGetTokenT<T>& token,const edm::ParameterSet& iPara,const std::string& tagName){
+    token = consumes<T>(iPara.getParameter<edm::InputTag>(tagName));
+  }
+};
+
+
+
+EcalClusIsoTreeMaker::EcalClusIsoTreeMaker(const edm::ParameterSet& iPara):
+  pfEcalClusTree_(nullptr)
+{
+  setToken(pfEcalIsoToken_,iPara,"pfEcalIsoTag");
+  setToken(pfEcalClusToken_,iPara,"pfEcalClusTag");
+  setToken(elesToken_,iPara,"elesTag");
+}
+
+EcalClusIsoTreeMaker::~EcalClusIsoTreeMaker()
+{
+
+}
+
+
+void EcalClusIsoTreeMaker::beginJob()
+{
+  edm::Service<TFileService> fs;
+  fs->file().cd();
+  pfEcalClusTree_ = new TTree("pfEcalClusTree","");
+  pfEcalClusData_.createBranches(pfEcalClusTree_);
+} 
+
+void EcalClusIsoTreeMaker::beginRun(const edm::Run& run,const edm::EventSetup& iSetup)
+{ 
+ 
+}
+
+namespace {
+  template<typename T> 
+  edm::Handle<T> getHandle(const edm::Event& iEvent,const edm::EDGetTokenT<T>& token)
+  {
+    edm::Handle<T> handle;
+    iEvent.getByToken(token,handle);
+    return handle;
+  }
+}
+
+
+void EcalClusIsoTreeMaker::analyze(const edm::Event& iEvent,const edm::EventSetup& iSetup)
+{
+  auto pfEcalIsoHandle = getHandle(iEvent,pfEcalIsoToken_);
+  auto pfEcalClusHandle = getHandle(iEvent,pfEcalClusToken_);
+  auto elesHandle = getHandle(iEvent,elesToken_);
+
+  for(size_t eleNr = 0; eleNr<elesHandle->size(); eleNr++){
+    edm::Ref<std::vector<reco::GsfElectron> > ele(elesHandle,eleNr);
+    pfEcalClusData_.fill(iEvent,ele,*pfEcalIsoHandle,pfEcalClusHandle);
+    pfEcalClusTree_->Fill();
+  }
+} 
+
+
+void EcalClusIsoTreeMaker::endJob()
+{ 
+
+}
+
+void PFEcalClusTreeStruct::createBranches(TTree* tree)
+{
+  tree->Branch("evt",&evt,evt.contents().c_str());
+  tree->Branch("seedId",&seedId,"seedId/i");
+  tree->Branch("eleEt",&eleEt,"eleEt/F");
+  tree->Branch("eleEta",&eleEta,"eleEta/F");
+  tree->Branch("elePhi",&elePhi,"elePhi/F");
+  tree->Branch("oldPFEcalIso",&oldPFEcalIso,"oldPFEcalIso/F");
+  tree->Branch("newPFEcalIso",&newPFEcalIso,"newPFEcalIso/F");
+ 
+}
+
+
+ 
+void PFEcalClusTreeStruct::fill(const edm::Event& event,edm::Ref<std::vector<reco::GsfElectron> > ele,const edm::ValueMap<float>& oldPfEcalIso,const edm::Handle<std::vector<reco::PFCluster> > newPFClusters)
+{
+  EcalPFClusterIsolation<reco::GsfElectron> pfClusIsoAlgo(0.3,0.,0.,0.,0.,0.,0.);
+  
+  evt.fill(event);
+  seedId = ele->superCluster()->seed()->seed().rawId();
+  eleEt = ele->et();
+  eleEta = ele->eta();
+  elePhi = ele->phi();
+  oldPFEcalIso = oldPfEcalIso[ele];
+  newPFEcalIso = pfClusIsoAlgo.getSum(ele,newPFClusters);
+}
+  
+
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(EcalClusIsoTreeMaker);
+  
+
---
+++ TrigNtup/test/pfEcalIsoTreeMaker.py
@@ -0,0 +1,298 @@
+isCrabJob=False #script seds this if its a crab job
+
+# Import configurations
+import FWCore.ParameterSet.Config as cms
+import os
+import sys
+# set up process
+process = cms.Process("HEEP")
+
+import FWCore.ParameterSet.VarParsing as VarParsing
+options = VarParsing.VarParsing ('analysis') 
+options.register('isMC',True,options.multiplicity.singleton,options.varType.bool," whether we are running on MC or not")
+options.register('pfRecHitThres',"",options.multiplicity.singleton,options.varType.string,'tag of pf rechit thresholds to apply')
+options.parseArguments()
+
+print options.inputFiles
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(options.inputFiles),  
+                          )
+
+
+# initialize MessageLogger and output report
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
+    reportEvery = cms.untracked.int32(5000),
+    limit = cms.untracked.int32(10000000)
+)
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+
+#Load geometry
+process.load("Configuration.Geometry.GeometryRecoDB_cff")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.autoCond import autoCond
+from Configuration.AlCa.GlobalTag import GlobalTag
+if options.isMC:
+#    process.GlobalTag = GlobalTag(process.GlobalTag, '102X_upgrade2018_realistic_v12', '')
+    process.GlobalTag = GlobalTag(process.GlobalTag, '103X_mc2017_realistic_v2', '')
+else:
+    from SHarper.SHNtupliser.globalTags_cfi import getGlobalTagNameData
+    globalTagName = getGlobalTagNameData(datasetVersion)
+    process.GlobalTag = GlobalTag(process.GlobalTag, globalTagName,'')
+    process.GlobalTag = GlobalTag(process.GlobalTag, '103X_dataRun2_v6_AC_v01', '')
+
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Geometry.CaloEventSetup.CaloTowerConstituents_cfi")
+process.load("Configuration.StandardSequences.Services_cff")
+
+# set the number of events
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents)
+)
+
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string(options.outputFile)
+)
+
+process.pfEcalIsoTreeMaker = cms.EDAnalyzer("EcalClusIsoTreeMaker",
+                                            elesTag = cms.InputTag("gedGsfElectrons"),
+                                            pfEcalIsoTag = cms.InputTag("electronEcalPFClusterIsolationProducer"),
+                                            pfEcalClusTag = cms.InputTag("particleFlowClusterECALUncorrected")
+                                            )
+
+
+process.particleFlowRecHitECAL = cms.EDProducer("PFRecHitProducer",
+    navigator = cms.PSet(
+        barrel = cms.PSet(
+
+        ),
+        endcap = cms.PSet(
+
+        ),
+        name = cms.string('PFRecHitECALNavigator')
+    ),
+    producers = cms.VPSet(
+        cms.PSet(
+            name = cms.string('PFEBRecHitCreator'),
+            qualityTests = cms.VPSet(
+                cms.PSet(
+                    applySelectionsToAllCrystals = cms.bool(True),
+                    name = cms.string('PFRecHitQTestDBThreshold')
+                ), 
+                cms.PSet(
+                    cleaningThreshold = cms.double(2.0),
+                    name = cms.string('PFRecHitQTestECAL'),
+                    skipTTRecoveredHits = cms.bool(True),
+                    timingCleaning = cms.bool(True),
+                    topologicalCleaning = cms.bool(True)
+                )
+            ),
+            srFlags = cms.InputTag(""),
+            src = cms.InputTag("reducedEcalRecHitsEB","")
+        ), 
+        cms.PSet(
+            name = cms.string('PFEERecHitCreator'),
+            qualityTests = cms.VPSet(
+                cms.PSet(
+                    applySelectionsToAllCrystals = cms.bool(True),
+                    name = cms.string('PFRecHitQTestDBThreshold')
+                ), 
+                cms.PSet(
+                    cleaningThreshold = cms.double(2.0),
+                    name = cms.string('PFRecHitQTestECAL'),
+                    skipTTRecoveredHits = cms.bool(True),
+                    timingCleaning = cms.bool(True),
+                    topologicalCleaning = cms.bool(True)
+                )
+            ),
+            srFlags = cms.InputTag(""),
+            src = cms.InputTag("reducedEcalRecHitsEE","")
+        )
+    )
+)
+
+process.particleFlowClusterECALUncorrected = cms.EDProducer("PFClusterProducer",
+    energyCorrector = cms.PSet(
+
+    ),
+    initialClusteringStep = cms.PSet(
+        algoName = cms.string('Basic2DGenericTopoClusterizer'),
+        thresholdsByDetector = cms.VPSet(
+            cms.PSet(
+                detector = cms.string('ECAL_BARREL'),
+                gatheringThreshold = cms.double(0.08),
+                gatheringThresholdPt = cms.double(0.0)
+            ), 
+            cms.PSet(
+                detector = cms.string('ECAL_ENDCAP'),
+                gatheringThreshold = cms.double(0.3),
+                gatheringThresholdPt = cms.double(0.0)
+            )
+        ),
+        useCornerCells = cms.bool(True)
+    ),
+    pfClusterBuilder = cms.PSet(
+        algoName = cms.string('Basic2DGenericPFlowClusterizer'),
+        allCellsPositionCalc = cms.PSet(
+            algoName = cms.string('Basic2DGenericPFlowPositionCalc'),
+            logWeightDenominator = cms.double(0.08),
+            minAllowedNormalization = cms.double(1e-09),
+            minFractionInCalc = cms.double(1e-09),
+            posCalcNCrystals = cms.int32(-1),
+            timeResolutionCalcBarrel = cms.PSet(
+                constantTerm = cms.double(0.428192),
+                constantTermLowE = cms.double(0.0),
+                corrTermLowE = cms.double(0.0510871),
+                noiseTerm = cms.double(1.10889),
+                noiseTermLowE = cms.double(1.31883),
+                threshHighE = cms.double(5.0),
+                threshLowE = cms.double(0.5)
+            ),
+            timeResolutionCalcEndcap = cms.PSet(
+                constantTerm = cms.double(0.0),
+                constantTermLowE = cms.double(0.0),
+                corrTermLowE = cms.double(0.0),
+                noiseTerm = cms.double(5.72489999999),
+                noiseTermLowE = cms.double(6.92683000001),
+                threshHighE = cms.double(10.0),
+                threshLowE = cms.double(1.0)
+            )
+        ),
+        excludeOtherSeeds = cms.bool(True),
+        maxIterations = cms.uint32(50),
+        minFracTot = cms.double(1e-20),
+        minFractionToKeep = cms.double(1e-07),
+        positionCalc = cms.PSet(
+            algoName = cms.string('Basic2DGenericPFlowPositionCalc'),
+            logWeightDenominator = cms.double(0.08),
+            minAllowedNormalization = cms.double(1e-09),
+            minFractionInCalc = cms.double(1e-09),
+            posCalcNCrystals = cms.int32(9),
+            timeResolutionCalcBarrel = cms.PSet(
+                constantTerm = cms.double(0.428192),
+                constantTermLowE = cms.double(0.0),
+                corrTermLowE = cms.double(0.0510871),
+                noiseTerm = cms.double(1.10889),
+                noiseTermLowE = cms.double(1.31883),
+                threshHighE = cms.double(5.0),
+                threshLowE = cms.double(0.5)
+            ),
+            timeResolutionCalcEndcap = cms.PSet(
+                constantTerm = cms.double(0.0),
+                constantTermLowE = cms.double(0.0),
+                corrTermLowE = cms.double(0.0),
+                noiseTerm = cms.double(5.72489999999),
+                noiseTermLowE = cms.double(6.92683000001),
+                threshHighE = cms.double(10.0),
+                threshLowE = cms.double(1.0)
+            )
+        ),
+        positionCalcForConvergence = cms.PSet(
+            T0_EB = cms.double(7.4),
+            T0_EE = cms.double(3.1),
+            T0_ES = cms.double(1.2),
+            W0 = cms.double(4.2),
+            X0 = cms.double(0.89),
+            algoName = cms.string('ECAL2DPositionCalcWithDepthCorr'),
+            minAllowedNormalization = cms.double(0.0),
+            minFractionInCalc = cms.double(0.0)
+        ),
+        recHitEnergyNorms = cms.VPSet(
+            cms.PSet(
+                detector = cms.string('ECAL_BARREL'),
+                recHitEnergyNorm = cms.double(0.08)
+            ), 
+            cms.PSet(
+                detector = cms.string('ECAL_ENDCAP'),
+                recHitEnergyNorm = cms.double(0.3)
+            )
+        ),
+        showerSigma = cms.double(1.5),
+        stoppingTolerance = cms.double(1e-08)
+    ),
+    positionReCalc = cms.PSet(
+        T0_EB = cms.double(7.4),
+        T0_EE = cms.double(3.1),
+        T0_ES = cms.double(1.2),
+        W0 = cms.double(4.2),
+        X0 = cms.double(0.89),
+        algoName = cms.string('ECAL2DPositionCalcWithDepthCorr'),
+        minAllowedNormalization = cms.double(0.0),
+        minFractionInCalc = cms.double(0.0)
+    ),
+    recHitCleaners = cms.VPSet(),
+    recHitsSource = cms.InputTag("particleFlowRecHitECAL"),
+    seedFinder = cms.PSet(
+        algoName = cms.string('LocalMaximumSeedFinder'),
+        nNeighbours = cms.int32(8),
+        thresholdsByDetector = cms.VPSet(
+            cms.PSet(
+                detector = cms.string('ECAL_ENDCAP'),
+                seedingThreshold = cms.double(0.6),
+                seedingThresholdPt = cms.double(0.15)
+            ), 
+            cms.PSet(
+                detector = cms.string('ECAL_BARREL'),
+                seedingThreshold = cms.double(0.23),
+                seedingThresholdPt = cms.double(0.0)
+            )
+        )
+    )
+)
+
+print process.GlobalTag.globaltag
+process.p = cms.Path(process.particleFlowRecHitECAL*
+                     process.particleFlowClusterECALUncorrected*
+                     process.pfEcalIsoTreeMaker)
+
+
+if options.pfRecHitThres != "" and options.pfRecHitThres != "NoThres":
+    from CondCore.DBCommon.CondDBSetup_cfi import *
+    process.pfRecHitThres = cms.ESSource("PoolDBESSource",CondDBSetup,
+                                         connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                                         toGet = cms.VPSet(cms.PSet(record = cms.string("EcalPFRecHitThresholdsRcd"),
+                                                                    tag = cms.string(options.pfRecHitThres)),
+                                                           )
+                                         )
+    if options.pfRecHitThres == "AB":       process.pfRecHitThres.toGet[0].tag = "EcalPFRecHitThresholds_UL_2017_mc_v1"
+    if options.pfRecHitThres == "AB1Sigma": process.pfRecHitThres.toGet[0].tag = "EcalPFRecHitThresholds_UL_2017_mc_v1"
+    if options.pfRecHitThres == "AC":       process.pfRecHitThres.toGet[0].tag = "EcalPFRecHitThresholds_UL_2017_mc_v2"
+    if options.pfRecHitThres == "AC1Sigma": process.pfRecHitThres.toGet[0].tag = "EcalPFRecHitThresholds_UL_2017_mc_v2"
+    if options.pfRecHitThres == "AC2Sigma": process.pfRecHitThres.toGet[0].tag = "EcalPFRecHitThresholds_UL_2017_mc_v2_2sigmas"
+    if options.pfRecHitThres == "AC3Sigma": process.pfRecHitThres.toGet[0].tag = "EcalPFRecHitThresholds_UL_2017_mc_v2_3sigmas"  
+    if options.pfRecHitThres == "AC4Sigma": process.pfRecHitThres.toGet[0].tag = "EcalPFRecHitThresholds_UL_2017_mc_v2_4sigmas"  
+
+    process.es_prefer_pfRecHitThres = cms.ESPrefer("PoolDBESSource","pfRecHitThres")
+    print "using custom ECAL rec-hit thresholds ",process.pfRecHitThres.toGet[0].tag.value()
+elif options.pfRecHitThres == "NoThres":
+    process.particleFlowRecHitECAL.producers[0].qualityTests = cms.VPSet( 
+        cms.PSet(
+            name = cms.string("PFRecHitQTestThreshold"),
+            threshold = cms.double(0.08)
+            ),
+        cms.PSet(
+            name = cms.string("PFRecHitQTestECAL"),
+            cleaningThreshold = cms.double(2.0),
+            timingCleaning = cms.bool(True),
+            topologicalCleaning = cms.bool(True),
+            skipTTRecoveredHits = cms.bool(True)
+            )
+        )
+    process.particleFlowRecHitECAL.producers[1].qualityTests = cms.VPSet( 
+        cms.PSet(
+            name = cms.string("PFRecHitQTestThreshold"),
+            threshold = cms.double(0.3)
+            ),
+        cms.PSet(
+            name = cms.string("PFRecHitQTestECAL"),
+            cleaningThreshold = cms.double(2.0),
+            timingCleaning = cms.bool(True),
+            topologicalCleaning = cms.bool(True),
+            skipTTRecoveredHits = cms.bool(True)
+            )
+        )
+    print "setting no additional thresholds on pfrechits"
+    print process.particleFlowRecHitECAL.dumpPython()
+    
