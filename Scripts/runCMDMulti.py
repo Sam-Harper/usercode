1400046a38d29bc45d6f14530b1a709e4fe4e60c
Sam Harper <sam.j.harper@gmail.com>
2019-03-15 12:35:35 +0000

    updated for better interface

--- Scripts/runCMDMulti.py
+++ Scripts/runCMDMulti.py
@@ -41,8 +41,9 @@ class JobThread (threading.Thread):
 import argparse
 parser = argparse.ArgumentParser(description='runs multiple instances of given cmd')
 parser.add_argument('cmd',help='cmd to run')
-parser.add_argument('--inputFiles',help='input file or file pattern to run over')
-parser.add_argument('--nrJobs',help='number of jobs to split into (can not be larger than #files to run over)',default=1,type=int)
+parser.add_argument('--inputFiles',help='input file or file pattern to run over, replaces %{inputFiles} string')
+parser.add_argument('--nrJobs',help='number of jobs to split into (can not be larger than #files to run over), replaces %{jobNr} string)',default=1,type=int)
+parser.add_argument('--seperator','-s',help='seperator of files on the cmd line, usually space or ,',default=' ',type=str)
 args = parser.parse_args()
 
 inputFilesEachJob=[""]*args.nrJobs
@@ -54,7 +55,9 @@ threads=[]
 for jobNr in range(1,args.nrJobs+1):
     inputFiles=""
     for inFile in inputFilesEachJob[jobNr%args.nrJobs]:
-        inputFiles+=inFile+" "
+        inputFiles+=inFile+args.seperator
+    inputFiles=inputFiles[:len(args.seperator)*-1] #chop the last seperator off
+    print inputFiles
     threads.append(JobThread(args,jobNr,inputFiles))
     
 for thread in threads:
