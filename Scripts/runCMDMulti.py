cab6f3e6f5ac553e2f15d5f016966e676a63c75a
Sam Harper <sam.j.harper@gmail.com>
2019-03-17 13:39:11 +0000

    adding max thread count to script

--- Scripts/runCMDMulti.py
+++ Scripts/runCMDMulti.py
@@ -7,7 +7,7 @@ def splitInput(inputFilesRAW,nrJobs):
         inputFiles= [line.strip() for line in open(inputFilesRAW)]
     else:
         inputFiles=glob.glob(inputFilesRAW)
-    print inputFiles
+#    print inputFiles
     inputFilesEachJob=nrJobs*[None];
 
     nrFiles=len(inputFiles)
@@ -38,11 +38,13 @@ class JobThread (threading.Thread):
         import subprocess
         output= subprocess.Popen(cmd.split(),stdout=subprocess.PIPE).communicate()[0].splitlines()
 
+import time
 import argparse
 parser = argparse.ArgumentParser(description='runs multiple instances of given cmd')
 parser.add_argument('cmd',help='cmd to run')
 parser.add_argument('--inputFiles',help='input file or file pattern to run over, replaces %{inputFiles} string')
 parser.add_argument('--nrJobs',help='number of jobs to split into (can not be larger than #files to run over), replaces %{jobNr} string)',default=1,type=int)
+parser.add_argument('--nrThreads','-t',type=int,default=-1,help='number of python threads to use')
 parser.add_argument('--seperator','-s',help='seperator of files on the cmd line, usually space or ,',default=' ',type=str)
 args = parser.parse_args()
 
@@ -50,15 +52,29 @@ inputFilesEachJob=[""]*args.nrJobs
 if args.inputFiles is not None:
     inputFilesEachJob = splitInput(args.inputFiles,args.nrJobs)
 
+if args.nrThreads <=0:
+    args.nrThreads = args.nrJobs
 
 threads=[]
 for jobNr in range(1,args.nrJobs+1):
+    while len(threads)>=args.nrThreads:
+      #  print "waiting for job {}".format(jobNr)
+        time.sleep(10)
+        for thread in threads:
+            if not thread.isAlive():
+                threads.remove(thread)
+    print "starting job {}, nr running {}".format(jobNr,len(threads))
     inputFiles=""
     for inFile in inputFilesEachJob[jobNr%args.nrJobs]:
         inputFiles+=inFile+args.seperator
     inputFiles=inputFiles[:len(args.seperator)*-1] #chop the last seperator off
-    print inputFiles
+   # print inputFiles
     threads.append(JobThread(args,jobNr,inputFiles))
+    threads[-1].start()
+
+while len(threads)!=0:
+    time.sleep(10)
+    for thread in threads:
+        if not thread.isAlive():
+            threads.remove(thread)
     
-for thread in threads:
-    thread.start()   
