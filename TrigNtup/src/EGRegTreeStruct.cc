13aeb631a87da1488546a9f6b652c0ae1be11e00
Sam Harper <sam.j.harper@gmail.com>
2019-05-29 20:53:27 +0100

    minor bug fixes iPhiMod20 and sigmaIEtaIPhi

--- TrigNtup/src/EGRegTreeStruct.cc
+++ TrigNtup/src/EGRegTreeStruct.cc
@@ -165,10 +165,9 @@ void SuperClustStruct::fill(const reco::SuperCluster& sc,const EcalChannelStatus
     const int iEtaCorr = ebDetId.ieta() - (ebDetId.ieta() > 0 ? +1 : -1);
     const int iEtaCorr26 = ebDetId.ieta() - (ebDetId.ieta() > 0 ? +26 : -26);
     iEtaMod5 = iEtaCorr%5;
-    //warning bug here!
     iEtaMod20 = std::abs(ebDetId.ieta())<=25 ? iEtaCorr%20 : iEtaCorr26%20;
     iPhiMod2 = (ebDetId.iphi()-1)%2;
-    iPhiMod20 = (ebDetId.iphi()-2)%20;
+    iPhiMod20 = (ebDetId.iphi()-1)%20;
     auto gapCode=[](int iEtaAbs){
       if(iEtaAbs==25 || iEtaAbs==45 || iEtaAbs==65 || iEtaAbs==85) return -1;//before gap
       else if(iEtaAbs==1 || iEtaAbs==26 || iEtaAbs==46 || iEtaAbs==66) return 1;//after gap
@@ -284,7 +283,7 @@ void ShowerShapeStruct::fill(const reco::GsfElectron::ShowerShape& eleSS,const r
   e2x5Left = eleSS.e2x5Left;
   e2x5Right = eleSS.e2x5Right;
   sigmaIEtaIEta = eleSS.sigmaIetaIeta;
-  sigmaIEtaIPhi = 0;
+  sigmaIEtaIPhi = eleSS.sigmaIetaIphi;
   sigmaIPhiIPhi = eleSS.sigmaIphiIphi;
 }
 
@@ -310,6 +309,6 @@ void ShowerShapeStruct::fill(const reco::Photon::ShowerShape& phoSS)
   e2x5Left = phoSS.e2x5Left;
   e2x5Right = phoSS.e2x5Right;
   sigmaIEtaIEta = phoSS.sigmaIetaIeta;
-  sigmaIEtaIPhi = 0;
+  sigmaIEtaIPhi = phoSS.sigmaIetaIphi;
   sigmaIPhiIPhi = phoSS.sigmaIphiIphi;
 }
